fc406f8a1df96f76897dae6cd29fe2675bc85343
kohkubo <kohkubo@student.42tokyo.jp>
2022-03-30 12:28:56 +0900

    test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-  if (argc != 2)
-  {
+  if (argc != 2) {
     std::cerr << "Usage: ./webserv <config file>" << std::endl;
     return (1);
   }
