a66a1d6891f9076ccf96107d58fd951bae74ef58
rakiyama <ryojiro25@gmail.com>
2022-04-11 21:12:22 +0900

    feat: add comment

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,19 +6,19 @@
 
 class Request {
 public:
-	Request(const std::string &message);
-	~Request() {}
-	std::string get_method() const { return __method_; }
-	std::string get_uri() const { return __uri_; }
-	Request(Request const &other);
-	Request &operator=(Request const &other);
+  Request(const std::string &message);
+  ~Request() {}
+  std::string get_method() const { return __method_; }
+  std::string get_uri() const { return __uri_; }
+  Request(Request const &other);
+  Request &operator=(Request const &other);
 
 private:
-	Request() {}
-	std::string					__method_;
-	std::string					__uri_;
-	std::string					__version_;
-	std::vector<std::string>	__fields_;
+  Request() {}
+  std::string              __method_;
+  std::string              __uri_;
+  std::string              __version_;
+  std::vector<std::string> __fields_;
 };
 
 #endif /* REQUEST_HPP */
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,31 +1,29 @@
 #include "Request.hpp"
 
 #define HTML_PATH "./html/index.html"
-#define METHOD "GET"
-#define VERSION "HTTP/1.1"
+#define METHOD    "GET"
+#define VERSION   "HTTP/1.1"
 
-Request::Request(const std::string &message)
-{
-	// lexer(message)
-	(void)message;
-	__method_ = METHOD;
-	__uri_ = HTML_PATH;
-	__version_ = VERSION;
-	__fields_.push_back("Host: example.com");
-	__fields_.push_back("User-Agent: curl/7.79.1");
-	__fields_.push_back("Accept: */*");
+/*
+ * recvで受け取った文字列をもとにパースする予定です。
+ * 今は決めうちで書いてます。
+ */
+Request::Request(const std::string &message) {
+  // lexer(message)
+  (void)message;
+  __method_  = METHOD;
+  __uri_     = HTML_PATH;
+  __version_ = VERSION;
+  __fields_.push_back("Host: example.com");
+  __fields_.push_back("User-Agent: curl/7.79.1");
+  __fields_.push_back("Accept: */*");
 }
 
-Request::Request(Request const &other)
-{
-	*this = other;
-}
+Request::Request(Request const &other) { *this = other; }
 
-Request &Request::operator=(Request const &other)
-{
-	if (this != &other)
-	{
-		/* copy */
-	}
-	return *this;
+Request &Request::operator=(Request const &other) {
+  if (this != &other) {
+    /* copy */
+  }
+  return *this;
 }
