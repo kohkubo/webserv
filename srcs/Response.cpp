652f32a09e9900b8f0c1e5416dc5d556db3394c3
rakiyama <ryojiro25@gmail.com>
2022-04-13 18:32:47 +0900

    リファクタ

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -9,31 +9,26 @@
 #define TEXT_STATUS_NOTFOUND "Not Found"
 #define VERSION              "HTTP/1.1"
 #define HELLO_WORLD_PAGE     "./html/index.html"
-#define ERR_PAGE             "./html/not_found.html"
+#define NOT_FOUND_PAGE       "./html/not_found.html"
 #define CR                   "\r"
 #define LF                   "\n"
 #define CRLF                 "\r\n"
 #define SP                   " "
 
 /*
- * コンストラクタ
- * リクエストメッセージをパース
- * 現時点: レキサーを使ってターゲットurlのみ確認してます.
- *        "/"ならindex.htmlを, それ以外ならエラー用のファイルを指定します.
+ * コンストラクタ: パースを省略
  */
 Response::Response(const std::string &request_message) {
   Lexer                 l(request_message, SP);
   Lexer::token_iterator it = l.begin();
 
-  // token: "GET", " ", "/", " ", "HTTP/1.1"...
-  __request_line_.push_back(*it);
+  __request_line_.push_back(*it); // "GET"
   it++;
   it++;
-  if (*it == "/")
-    __request_line_.push_back(HELLO_WORLD_PAGE);
-  else
-    __request_line_.push_back(ERR_PAGE);
-  __request_line_.push_back(VERSION);
+  __request_line_.push_back(*it); // "/"
+  it++;
+  it++;
+  __request_line_.push_back(*it); // "HTTP/1.1"
 
   __request_field_.push_back("Host: example.com");
   __request_field_.push_back("User-Agent: curl/7.79.1");
@@ -49,11 +44,13 @@ void Response::process() {
 
   // 対象ファイルから内容読み込み
   std::string target_path = __request_line_[1];
+  if (target_path == "/")
+    target_path = HELLO_WORLD_PAGE;
   if (is_file_exists(target_path.c_str())) {
     __status_line_.push_back(STATUS_OK);
     __status_line_.push_back(TEXT_STATUS_OK);
   } else {
-    target_path = ERR_PAGE;
+    target_path = NOT_FOUND_PAGE;
     __status_line_.push_back(STATUS_NOTFOUND);
     __status_line_.push_back(TEXT_STATUS_NOTFOUND);
   }
