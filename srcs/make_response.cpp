6e4e8d0ef7f16e77fa699c55be3786665a00e58d
rakiyama <ryojiro25@gmail.com>
2022-04-15 18:50:04 +0900

    make method_get

--- srcs/make_response.cpp
+++ srcs/make_response.cpp
@@ -1,55 +1,56 @@
 #include "message.hpp"
 #include "util.hpp"
 #include <iostream>
+#include <map>
 
 /*
  * サーバー処理
- * GETのみ対応.
- * 挿入時keyが被っている時の処理は現状考慮してない.
+ * メソッドはGETであることが前提.
+ * mapへの挿入時keyが被っている時の処理は現状考慮してない.
  * レスポンスに必要な要素を埋める.
  */
-void process() {
+void method_get(std::map<std::string, std::string> request, std::map<std::string, std::string> response) {
   /* 対象ファイルから内容読み込み */
-  std::string target_url = request_[URL];
+  std::string target_url = request[URL];
   if (is_file_exists(target_url.c_str())) {
-    response_[STATUS] = STATUS_OK;
-    response_[PHRASE] = PHRASE_STATUS_OK;
+    response[STATUS] = STATUS_OK;
+    response[PHRASE] = PHRASE_STATUS_OK;
   } else {
-    target_url          = NOT_FOUND_PAGE;
-    response_[STATUS] = STATUS_NOTFOUND;
-    response_[PHRASE] = PHRASE_STATUS_NOTFOUND;
+    target_url       = NOT_FOUND_PAGE;
+    response[STATUS] = STATUS_NOTFOUND;
+    response[PHRASE] = PHRASE_STATUS_NOTFOUND;
   }
   std::string content       = read_file_tostring(target_url.c_str());
-  response_[BODY]         = content;
-  response_[CONTENT_LEN]  = to_string(content.size());
+  response[BODY]         = content;
+  response[CONTENT_LEN]  = to_string(content.size());
 
   /* その他必要なものをレスポンスに追加 */
-  response_[VERSION]      = VERSION_HTTP;
-  response_[CONTENT_TYPE] = TEXT_HTML;
-  response_[CONNECTION]   = CONNECTION_CLOSE;
-}
-
-std::string message() {
-  return status_line_message() + field_message() + CRLF + body_message();
+  response[VERSION]      = VERSION_HTTP;
+  response[CONTENT_TYPE] = TEXT_HTML;
+  response[CONNECTION]   = CONNECTION_CLOSE;
 }
 
 /* ステータスラインの要素は必須だが, 存在しなかった時のバリデートは現状してない
  */
-std::string status_line_message() {
+static std::string status_line_message() {
   return response_[VERSION] + SP + response_[STATUS] + SP +
          response_[PHRASE] + CRLF;
 }
 
-std::string field_message() {
+static std::string field_message() {
   return field_line(HOST) + field_line(CONTENT_LEN) +
          field_line(CONTENT_TYPE) + field_line(CONNECTION);
 }
 
-std::string field_line(std::string key) {
-  if (request_.find(key) == request_.end())
+static std::string field_line(static std::string key) {
+  if (request.find(key) == request.end())
     return "";
   else
-    return key + ": " + request_[key] + CRLF;
+    return key + ": " + request[key] + CRLF;
 }
 
-std::string body_message() { return response_[BODY]; }
+static std::string body_message() { return response_[BODY]; }
+
+std::string response_message() {
+  return status_line_message() + field_message() + CRLF + body_message();
+}
--- srcs/parse_request.cpp
+++ srcs/parse_request.cpp
@@ -1,25 +1,28 @@
 #include "message.hpp"
 #include "Lexer.hpp"
 #include <iostream>
+#include <map>
 
 /*
  * 超安易パース
+ * リクエストメッセージのターゲットURLが"/"の時index.htmlに差し替えることだけしています.
  * mapへの挿入時keyが被っている時の処理は現状考慮してない.
  */
-void parse_request(Lexer &message_lexer) {
-  Lexer::token_iterator it = message_lexer.begin();
-  request_[METHOD]       = *it; // *it = "GET"
+std::map<std::string, std::string> parse_request(Lexer &request_message) {
+  Lexer::token_iterator it = request_message.begin();
+  std::map<std::string, std::string>  request;
+  request[METHOD]       = *it; // *it = "GET"
   it++;
   it++;
   std::string target_path = *it; // *it = "/"
   if (target_path == "/")
     target_path = HELLO_WORLD_PAGE;
-  request_[URL] = target_path;
+  request[URL] = target_path;
   it++;
   it++;
-  request_[VERSION]   = *it; // *it = "HTTP/1.1"
-
-  request_[HOST]      = "example.com";
-  request_[USERAGENT] = "curl/7.79.1";
-  request_[ACCEPT]    = "*/*";
+  request[VERSION]   = *it; // *it = "HTTP/1.1"
+  request[HOST]      = "example.com";
+  request[USERAGENT] = "curl/7.79.1";
+  request[ACCEPT]    = "*/*";
+  return request;
 }
