d044b0ff1d58ce42cce19c6f48bb2bacdd867d74
rakiyama <ryojiro25@gmail.com>
2022-04-17 19:14:11 +0900

    done: make response()

--- srcs/make_response.cpp
+++ srcs/make_response.cpp
@@ -9,7 +9,7 @@
  * mapへの挿入時keyが被っている時の処理は現状考慮してない.
  * レスポンスに必要な要素を埋める.
  */
-void method_get(std::map<std::string, std::string> request, std::map<std::string, std::string> response) {
+void method_get(std::map<std::string, std::string> &request, std::map<std::string, std::string> &response) {
   /* 対象ファイルから内容読み込み */
   std::string target_url = request[URL];
   if (is_file_exists(target_url.c_str())) {
@@ -32,25 +32,25 @@ void method_get(std::map<std::string, std::string> request, std::map<std::string
 
 /* ステータスラインの要素は必須だが, 存在しなかった時のバリデートは現状してない
  */
-static std::string status_line_message() {
-  return response_[VERSION] + SP + response_[STATUS] + SP +
-         response_[PHRASE] + CRLF;
+static std::string status_line_message(std::map<std::string, std::string> &response) {
+  return response[VERSION] + SP + response[STATUS] + SP +
+         response[PHRASE] + CRLF;
 }
 
-static std::string field_message() {
-  return field_line(HOST) + field_line(CONTENT_LEN) +
-         field_line(CONTENT_TYPE) + field_line(CONNECTION);
-}
-
-static std::string field_line(static std::string key) {
-  if (request.find(key) == request.end())
+static std::string field_line(std::map<std::string, std::string> &response, const std::string key) {
+  if (response.find(key) == response.end())
     return "";
   else
-    return key + ": " + request[key] + CRLF;
+    return key + ": " + response[key] + CRLF;
 }
 
-static std::string body_message() { return response_[BODY]; }
+static std::string field_message(std::map<std::string, std::string> &response) {
+  return field_line(response, HOST) + field_line(response, CONTENT_LEN) +
+         field_line(response, CONTENT_TYPE) + field_line(response, CONNECTION);
+}
 
-std::string response_message() {
-  return status_line_message() + field_message() + CRLF + body_message();
+static std::string body_message(std::map<std::string, std::string> &response) { return response[BODY]; }
+
+std::string response_message(std::map<std::string, std::string> &response) {
+  return status_line_message(response) + field_message(response) + CRLF + body_message(response);
 }
