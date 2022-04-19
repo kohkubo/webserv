118d41d94ee8497f6c7b6c18e84495fd4ac5f38a
kohkubo <kohkubo@student.42tokyo.jp>
2022-04-19 14:10:13 +0900

    変数名

--- srcs/http/parse.cpp
+++ srcs/http/parse.cpp
@@ -8,19 +8,19 @@
  */
 http_message_map http_parse_request_message(Lexer &request_lexer) {
   Lexer::token_iterator it = request_lexer.begin();
-  http_message_map      http_request_message;
-  http_request_message[METHOD] = *it; // *it = "http_method_read_get"
+  http_message_map      request_message;
+  request_message[METHOD] = *it; // *it = "http_method_read_get"
   it++;
   it++;
   std::string target_path = *it; // *it = "/"
   if (target_path == "/")
     target_path = HELLO_WORLD_PAGE;
-  http_request_message[URL] = target_path;
+  request_message[URL] = target_path;
   it++;
   it++;
-  http_request_message[VERSION]   = *it; // *it = "HTTP/1.1"
-  http_request_message[HOST]      = "example.com";
-  http_request_message[USERAGENT] = "curl/7.79.1";
-  http_request_message[ACCEPT]    = "*/*";
-  return http_request_message;
+  request_message[VERSION]   = *it; // *it = "HTTP/1.1"
+  request_message[HOST]      = "example.com";
+  request_message[USERAGENT] = "curl/7.79.1";
+  request_message[ACCEPT]    = "*/*";
+  return request_message;
 }
