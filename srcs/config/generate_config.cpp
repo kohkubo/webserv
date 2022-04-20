0058f1f359cc59f341733a316c5ddcef912b1c02
rakiyama <ryojiro25@gmail.com>
2022-04-20 21:36:27 +0900

    define

--- srcs/config/generate_config.cpp
+++ srcs/config/generate_config.cpp
@@ -3,9 +3,12 @@
 #include "util.hpp"
 #include <iostream>
 
+#define DELIMITER "\v\r\f\t\n {};"
+#define SKIP "\v\r\f\t\n "
+
 Config *generate_config(const char *config_file_path) {
   std::string config_content = read_file_tostring(config_file_path);
-  Tokens t(config_content, "\v\r\f\t\n " "{};", "\v\r\f\t\n ");
+  Tokens t(config_content, DELIMITER, SKIP);
   try
   {
     Config *c = new Config(t);
