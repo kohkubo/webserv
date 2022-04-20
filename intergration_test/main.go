906871c559646ee9f149a550acfa21119d84aad6
rakiyama <ryojiro25@gmail.com>
2022-04-20 19:49:46 +0900

    gettestを分けた

--- intergration_test/main.go
+++ intergration_test/main.go
@@ -10,14 +10,6 @@ import (
 	"os"
 )
 
-// ヘッダーは中身なし
-//http.NewRequest("GET", "http://localhost:5001", nil)
-//中身:
-//GET / HTTP/1.1
-//Host: localhost:5001
-//User-Agent: Go-http-client/1.1
-//Accept-Encoding: gzip
-
 //http.NewRequest("POST", "http://localhost:5001", nil)
 //中身:
 //POST / HTTP/1.1
@@ -34,48 +26,83 @@ import (
 //Accept-Encoding: gzip
 
 // get, post, deleteでテストを分ける(postではタイプが必要か？)
-// デフォルトで追加されるフィールドに関してまとめておく
-// 追加のフィールドはrangeで追加する
-// テーブルで実装する
+const (
+	index_html = "../html/index.html"
+)
+
 func main() {
-	req, err := http.NewRequest("DELETE", "http://localhost:5001", nil)
+	TestGet()
+}
+
+// デフォルトで作成されるリクエスト
+// リクエスト:
+//  http.NewRequest("GET", "http://localhost:5001", nil)
+// 中身:
+//  GET / HTTP/1.1
+//  Host: localhost:5001
+//  User-Agent: Go-http-client/1.1
+//  Accept-Encoding: gzip
+func TestGet() {
+	tests := []struct {
+		name           string
+		uri            string
+		addFields      map[string]string
+		wantStatusCode int
+		wantBody       []byte
+	}{
+		{
+			name:           "root",
+			uri:            "http://localhost:5001",
+			addFields:      nil,
+			wantStatusCode: http.StatusOK,
+			wantBody:       FileBytes(index_html),
+		},
+	}
+	fmt.Println("GET test")
+	for _, tt := range tests {
+		tt := tt
+		func() {
+			resp := Request("GET", tt.uri, tt.addFields, nil)
+			defer resp.Body.Close()
+			if resp.StatusCode != tt.wantStatusCode {
+				fmt.Printf("["+tt.name+" status]"+"actual: %v, expect: %v", resp.StatusCode, tt.wantStatusCode)
+			}
+			bodyResponsed, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("fail to get read body: %v", err)
+			}
+			if bytes.Compare(bodyResponsed, tt.wantBody) != 0 {
+				fmt.Printf("["+tt.name+" body]"+"actual: %v, expect: %v", resp.StatusCode, tt.wantStatusCode)
+			}
+		}()
+	}
+}
+
+func Request(method string, uri string, addFields map[string]string, body io.Reader) *http.Response {
+	req, err := http.NewRequest("GET", uri, body)
 	if err != nil {
 		log.Fatalf("fail to send request: %v", err)
 	}
-	//req.Header.Add("Range", "a")
-	//for k, v := range req.Header {
-	//	fmt.Println("--->", k, v)
-	//}
+	for key, value := range addFields {
+		req.Header.Add(key, value)
+	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalf("fail to get response: %v", err)
 	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("fail to get read body: %v", err)
-	}
-	expect, err := FileBytes("../html/index.html")
-	if err != nil {
-		log.Fatalf("fail to get file bytes: %v", err)
-	}
-	if bytes.Compare(body, expect) != 0 {
-		log.Fatal("error")
-	} else {
-		fmt.Println("ok")
-	}
+	return resp
 }
 
-func FileBytes(fileName string) ([]byte, error) {
+func FileBytes(fileName string) []byte {
 	file, err := os.Open(fileName)
 	if err != nil {
-		return nil, fmt.Errorf("FileBytes: %v", err)
+		log.Fatalf("FileBytes: %v", err)
 	}
 	defer file.Close()
 	srcBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		return nil, fmt.Errorf("FileBytes: %v", err)
+		log.Fatalf("FileBytes: %v", err)
 	}
-	return srcBytes, nil
+	return srcBytes
 }
