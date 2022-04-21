edf367e2481219b9f5cd9788e33e1296700d5a65
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-21 13:31:10 +0900

    tokenize関数追加

--- googletest/tests/token_test.cpp
+++ googletest/tests/token_test.cpp
@@ -1,15 +1,15 @@
 #include "gtest/gtest.h"
 
-#include "Tokens.hpp"
+#include "util.hpp"
 
 #include <iostream>
 #include <string>
 
 #define SPACES "\v\r\f\t\n "
 
-TEST(tokens_test, simple_split) {
-  Tokens                 l("apple,orange,banana", ",", SPACES);
-  Tokens::token_iterator it = l.begin();
+TEST(tokenize_test, simple_split) {
+  std::vector<std::string> l = tokenize("apple,orange,banana", ",", SPACES);
+  std::vector<std::string>::iterator it = l.begin();
   EXPECT_EQ(*it++, std::string("apple"));
   EXPECT_EQ(*it++, std::string(","));
   EXPECT_EQ(*it++, std::string("orange"));
@@ -18,16 +18,16 @@ TEST(tokens_test, simple_split) {
   EXPECT_EQ(it, l.end());
 }
 
-TEST(tokens_test, no_delimiter) {
-  Tokens                 l("apple,orange,banana", " ", SPACES);
-  Tokens::token_iterator it = l.begin();
+TEST(tokenize_test, no_delimiter) {
+  std::vector<std::string> l = tokenize("apple,orange,banana", " ", SPACES);
+  std::vector<std::string>::iterator it = l.begin();
   EXPECT_EQ(*it++, std::string("apple,orange,banana"));
   EXPECT_EQ(it, l.end());
 }
 
-TEST(tokens_test, multi_delimiter) {
-  Tokens                 l("apple, orange, banana", " ,", SPACES);
-  Tokens::token_iterator it = l.begin();
+TEST(tokenize_test, multi_delimiter) {
+  std::vector<std::string> l = tokenize("apple, orange, banana", " ,", SPACES);
+  std::vector<std::string>::iterator it = l.begin();
   EXPECT_EQ(*it++, std::string("apple"));
   EXPECT_EQ(*it++, std::string(","));
   EXPECT_EQ(*it++, std::string("orange"));
@@ -36,9 +36,10 @@ TEST(tokens_test, multi_delimiter) {
   EXPECT_EQ(it, l.end());
 }
 
-TEST(tokens_test, skip_front_back) {
-  Tokens                 l("\n apple, orange, banana \n", " ,", SPACES);
-  Tokens::token_iterator it = l.begin();
+TEST(tokenize_test, skip_front_back) {
+  std::vector<std::string> l =
+      tokenize("\n apple, orange, banana \n", " ,", SPACES);
+  std::vector<std::string>::iterator it = l.begin();
   EXPECT_EQ(*it++, std::string("apple"));
   EXPECT_EQ(*it++, std::string(","));
   EXPECT_EQ(*it++, std::string("orange"));
@@ -47,9 +48,9 @@ TEST(tokens_test, skip_front_back) {
   EXPECT_EQ(it, l.end());
 }
 
-TEST(tokens_test, delimiter) {
-  Tokens                 l("{{}}{}{,}", "{},", SPACES);
-  Tokens::token_iterator it = l.begin();
+TEST(tokenize_test, delimiter) {
+  std::vector<std::string>           l  = tokenize("{{}}{}{,}", "{},", SPACES);
+  std::vector<std::string>::iterator it = l.begin();
   EXPECT_EQ(*it++, std::string("{"));
   EXPECT_EQ(*it++, std::string("{"));
   EXPECT_EQ(*it++, std::string("}"));
@@ -62,15 +63,15 @@ TEST(tokens_test, delimiter) {
   EXPECT_EQ(it, l.end());
 }
 
-TEST(tokens_test, config) {
-  std::string            str = R"(server {
+TEST(tokenize_test, config) {
+  std::string                        str  = R"(server {
   listen 127.0.0.1:80;
   location /hoge {
     root /var/fuga;
   }
 })";
-  Tokens                 test(str, SPACES "{};", SPACES);
-  Tokens::token_iterator it = test.begin();
+  std::vector<std::string>           test = tokenize(str, SPACES "{};", SPACES);
+  std::vector<std::string>::iterator it   = test.begin();
   EXPECT_EQ(*it++, std::string("server"));
   EXPECT_EQ(*it++, std::string("{"));
   EXPECT_EQ(*it++, std::string("listen"));
@@ -87,15 +88,15 @@ TEST(tokens_test, config) {
   EXPECT_EQ(it, test.end());
 }
 
-TEST(tokens_test, httpReqest) {
-  std::string            str = "POST /search.html HTTP/1.1\r\n"
-                               "Host: wa3.i-3-i.info\r\n"
-                               "Connection: keep-alive\r\n"
-                               "\r\n"
-                               "body";
+TEST(tokenize_test, httpReqest) {
+  std::string                        str  = "POST /search.html HTTP/1.1\r\n"
+                                            "Host: wa3.i-3-i.info\r\n"
+                                            "Connection: keep-alive\r\n"
+                                            "\r\n"
+                                            "body";
 
-  Tokens                 test(str, "\r\n:", " ");
-  Tokens::token_iterator it = test.begin();
+  std::vector<std::string>           test = tokenize(str, "\r\n:", " ");
+  std::vector<std::string>::iterator it   = test.begin();
   EXPECT_EQ(*it++, std::string("POST /search.html HTTP/1.1"));
   EXPECT_EQ(*it++, std::string("\r"));
   EXPECT_EQ(*it++, std::string("\n"));
