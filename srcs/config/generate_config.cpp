f6e9b99a9571a6716ad0273666a4643454bb0910
rakiyama <ryojiro25@gmail.com>
2022-04-21 11:43:39 +0900

    clang-format

--- srcs/config/generate_config.cpp
+++ srcs/config/generate_config.cpp
@@ -1,21 +1,19 @@
-#include "config/Config.hpp"
 #include "Tokens.hpp"
+#include "config/Config.hpp"
 #include "util.hpp"
 #include <iostream>
 
 #define DELIMITER "\v\r\f\t\n {};"
-#define SKIP "\v\r\f\t\n "
+#define SKIP      "\v\r\f\t\n "
 
 Config *generate_config(const char *config_file_path) {
-  std::string config_content = read_file_tostring(config_file_path);// TODO: エラー受け取る
+  std::string config_content =
+      read_file_tostring(config_file_path); // TODO: エラー受け取る
   Tokens t(config_content, DELIMITER, SKIP);
-  try
-  {
+  try {
     Config *c = new Config(t);
     return c;
-  }
-  catch(const std::exception& e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
     return NULL;
   }
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,17 +6,16 @@
 
 int main(int argc, char **argv) {
   std::string config_file_path;
-  switch (argc)
-  {
-    case 1:
-      config_file_path = DEFAULT_CONFIG_FILE_PATH;
-      break;
-    case 2:
-      config_file_path = argv[1];
-      break;
-    default:
-      std::cerr << "Usage: ./webserv [config file]" << std::endl;
-      return (1);
+  switch (argc) {
+  case 1:
+    config_file_path = DEFAULT_CONFIG_FILE_PATH;
+    break;
+  case 2:
+    config_file_path = argv[1];
+    break;
+  default:
+    std::cerr << "Usage: ./webserv [config file]" << std::endl;
+    return (1);
   }
   Config *c = generate_config(config_file_path.c_str());
   if (c == NULL)
