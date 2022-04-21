874d2a76500f1ac1e64b7bde0ee9a1380cb093a0
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-21 23:19:05 +0900

    comment out

--- srcs/event/listen_event.cpp
+++ srcs/event/listen_event.cpp
@@ -1,49 +1,49 @@
 
-#include "Socket.hpp"
-#include "config/ServerConfig.hpp"
-#include "event.hpp"
+// #include "Socket.hpp"
+// #include "config/ServerConfig.hpp"
+// #include "event.hpp"
 
-void listen_event(const std::vector<ServerConfig> server_list) {
-  // 必要なもの
-  // serverConfigのリスト
-  // チェックしたいfdのリスト
-  std::deque<int> socket_fd;
-  std::deque<int> connection_fd;
+// void listen_event(const std::vector<ServerConfig> server_list) {
+//   // 必要なもの
+//   // serverConfigのリスト
+//   // チェックしたいfdのリスト
+//   std::deque<int> socket_fd;
+//   std::deque<int> connection_fd;
 
-  // server_listのportに対してsocketを開く。 + socketfdをkeyにしたmapを作る。
-  Socket         *socket = new Socket(server_config);
-  while (1) {
-    // socket_fd + connection_fd（接続済みのsocket）
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(socket->get_listenfd(), &readfds);
+//   // server_listのportに対してsocketを開く。 + socketfdをkeyにしたmapを作る。
+//   // Socket         *socket = new Socket(server_config);
+//   while (1) {
+//     // socket_fd + connection_fd（接続済みのsocket）
+//     fd_set readfds;
+//     FD_ZERO(&readfds);
+//     FD_SET(socket->get_listenfd(), &readfds);
 
-    timeval timeout = {.tv_sec = 0, .tv_usec = 0};
+//     timeval timeout = {.tv_sec = 0, .tv_usec = 0};
 
-    int     ret =
-        select(socket->get_listenfd() + 1, &readfds, NULL, NULL, &timeout);
-    if (ret == -1) {
-      error_log_with_errno("select() failed. readfds.");
-      continue;
-    }
-    int i = 0;
-    while (i < ret) {
-      // socket_fd -> acceptしてconnection_fdに加える。
-      if (FD_ISSET(socket_fd[0], &readfds)) {
-        handle_connection();
-      }
-      // connection_fd -> リクエストのデータがある。
-      if (FD_ISSET(connection_fd[0], &readfds)) {
-        handle_request();
-      }
-    }
+//     int     ret =
+//         select(socket->get_listenfd() + 1, &readfds, NULL, NULL, &timeout);
+//     if (ret == -1) {
+//       error_log_with_errno("select() failed. readfds.");
+//       continue;
+//     }
+//     int i = 0;
+//     while (i < ret) {
+//       // socket_fd -> acceptしてconnection_fdに加える。
+//       if (FD_ISSET(socket_fd[0], &readfds)) {
+//         // handle_connection();
+//       }
+//       // connection_fd -> リクエストのデータがある。
+//       if (FD_ISSET(connection_fd[0], &readfds)) {
+//         // handle_request();
+//       }
+//     }
 
-    if (ret && FD_ISSET(socket->get_listenfd(), &readfds)) {
-      int accfd = accept(socket->get_listenfd(), (struct sockaddr *)NULL, NULL);
-      if (accfd == -1) {
-        continue;
-      }
-    }
-  }
-  return;
-}
+//     if (ret && FD_ISSET(socket->get_listenfd(), &readfds)) {
+//       int accfd = accept(socket->get_listenfd(), (struct sockaddr *)NULL,
+//       NULL); if (accfd == -1) {
+//         continue;
+//       }
+//     }
+//   }
+//   return;
+// }
