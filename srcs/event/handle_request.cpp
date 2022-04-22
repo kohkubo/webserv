e3cb79d6e7a616443a0b5418bf257dc26cede8cd
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-22 11:46:30 +0900

    コメント追加

--- srcs/event/handle_request.cpp
+++ srcs/event/handle_request.cpp
@@ -4,7 +4,9 @@
   データを受信しているfdからデータを読み込み、既存の受信データがある場合データを追加する。
   もしrecvの返り値が0だった場合（コネクションが閉じている）、
   受信したデータをhttpリクエストとしてパース、対応したレスポンスを作成する。
+  閉じたコネクションはconnection_listから削除する。
   connection_listはsocket、受信データ(string)を含んだクラスのほうが良いかも。
+
 */
 
 void handle_request(int connection, const std::map<int, Socket> &socket_list,
