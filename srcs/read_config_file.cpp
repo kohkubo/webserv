55011dfee833395068404ba39234d2d84bd2ece2
rakiyama <ryojiro25@gmail.com>
2022-04-22 20:42:59 +0900

    変数名変更

--- srcs/read_config_file.cpp
+++ srcs/read_config_file.cpp
@@ -15,18 +15,18 @@ typedef std::map<int, server_config_vector> server_config_map;
 ServerConfig read_config_file(const char *config_file_path) {
   std::string config_content =
       read_file_tostring(config_file_path); // TODO: エラー受け取る
-  std::vector<std::string> l = tokenize(config_content, DELIMITER, SKIP);// 変数名しっかり
-  server_config_vector  server_config;// 変数名しっかり
-  ServerConfig          server_config_tmp;// 変数名しっかり
+  std::vector<std::string> config_tokens = tokenize(config_content, DELIMITER, SKIP);
+  server_config_vector  server_list;
   try {
-    std::vector<std::string>::iterator it = l.begin();
-    while (it != l.end()) {
+    std::vector<std::string>::iterator it = config_tokens.begin();
+    while (it != config_tokens.end()) {
       if (*it == "server") {
-        it = server_config_tmp.parse(it, l.end());
-        server_config.push_back(server_config_tmp);
+        ServerConfig          new_server;
+        it = new_server.parse(it, config_tokens.end());
+        server_list.push_back(new_server);
       }
     }
-    return server_config[0];// TODO: server_configを返す
+    return server_list[0];// TODO: server_listを返す
   } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
     exit(EXIT_FAILURE); // 一旦
