068784c2aaa1b54a47712e82e311838172fa7d6c
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-22 14:17:45 +0900

    create_socket_list wip

--- srcs/event/create_socket_map.cpp
+++ srcs/event/create_socket_map.cpp
@@ -1,4 +1,4 @@
-#include "Socket.hpp"
+#include "event.hpp"
 
 #include <map>
 
@@ -6,9 +6,17 @@
 ** ServerConfigのリストからソケットを作成、ソケットのfdをkeyにしたSocketクラスのmapを作る。
 ** n^2になりそう。
 */
-std::map<int, Socket>
+std::map<socket_fd, std::vector<Socket>>
 create_socket_map(const std::vector<ServerConfig> &server_list) {
-  std::map<int, Socket> res;
+  std::map<socket_fd, std::vector<Socket>>  res;
+
+  // tmpにlistenディレクティブが共通するServerConfigのリストを作る。
+  std::vector<std::vector<ServerConfig>>    tmp;
+  std::vector<ServerConfig>::const_iterator it = server_list.begin();
+  for (; it != server_list.end(); it++) {
+  }
+
+  // tmpからsocketを開いて、fdをkeyにしたmapにする。
 
   return res;
 }
