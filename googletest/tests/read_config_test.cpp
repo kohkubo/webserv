2c43ba6a3baeee644d46d8959b0e14edf733bdb0
rakiyama <ryojiro25@gmail.com>
2022-04-24 00:14:02 +0900

    テスト追加

---
+++ googletest/tests/read_config_test.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include "ServerConfig.hpp"
+#include "Webserv.hpp"
+#include "gtest/gtest.h"
+
+#define SAMPLE_CONF "../googletest/tdata/sample.conf"
+
+TEST(read_config_test, read_server) {
+  std::vector<ServerConfig> server_list = read_server_configuration(SAMPLE_CONF);
+
+  EXPECT_EQ(server_list[0].listen_address_, std::string("0.0.0.0"));
+  EXPECT_EQ(server_list[0].listen_port_, std::string("5500"));
+
+  EXPECT_EQ(server_list[1].listen_address_, std::string("0.0.0.0"));
+  EXPECT_EQ(server_list[1].listen_port_, std::string("5500"));
+
+  EXPECT_EQ(server_list[2].listen_address_, std::string("0.0.0.0"));
+  EXPECT_EQ(server_list[2].listen_port_, std::string("5001"));
+}
