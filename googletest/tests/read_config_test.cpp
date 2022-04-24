9ada447b960f2d5f1224f700e39c394b859f7d4c
rakiyama <ryojiro25@gmail.com>
2022-04-24 19:43:51 +0900

    parse_conf 追加

--- googletest/tests/read_config_test.cpp
+++ googletest/tests/read_config_test.cpp
@@ -8,12 +8,16 @@
 TEST(read_config_test, simple_test) {
   std::vector<ServerConfig> server_list = read_config(SAMPLE_CONF);
 
-  EXPECT_EQ(server_list[0].listen_address_, std::string("0.0.0.0"));
-  EXPECT_EQ(server_list[0].listen_port_, std::string("5500"));
+  EXPECT_EQ(server_list[0].listen_address_, "0.0.0.0");
+  EXPECT_EQ(server_list[0].listen_port_, "5500");
+  EXPECT_EQ(server_list[0].server_name_[0], "example.com");
+  EXPECT_EQ(server_list[0].server_name_[1], "example.net");
 
-  EXPECT_EQ(server_list[1].listen_address_, std::string("0.0.0.0"));
-  EXPECT_EQ(server_list[1].listen_port_, std::string("5500"));
+  EXPECT_EQ(server_list[1].listen_address_, "0.0.0.0");
+  EXPECT_EQ(server_list[1].listen_port_, "5500");
+  EXPECT_EQ(server_list[1].root_, "/tmp/www");
 
-  EXPECT_EQ(server_list[2].listen_address_, std::string("0.0.0.0"));
-  EXPECT_EQ(server_list[2].listen_port_, std::string("5001"));
+  EXPECT_EQ(server_list[2].listen_address_, "0.0.0.0");
+  EXPECT_EQ(server_list[2].listen_port_, "5001");
+  EXPECT_EQ(server_list[0].server_name_[0], "example.com");
 }
