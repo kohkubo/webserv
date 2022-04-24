6436fedaed84f132a085145d3a4a555843ce3cb2
rakiyama <ryojiro25@gmail.com>
2022-04-24 10:46:49 +0900

    ディレクトリ名変更

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 webserv="./webserv"
-test="./intergration_test/integration_test"
+testdir="./integration_test"
 
-(./webserv > /dev/null 2>&1) &
+($webserv > /dev/null 2>&1) &
 webserv_pid=`ps | grep webserv | sed '/grep/d' | awk '{print $1}'`
 
-make run -C ./intergration_test 
+make run -C $testdir
 
 kill $webserv_pid
