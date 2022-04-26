d799cb1adf05c0cf85c0066cf35aa2284d322392
rakiyama <ryojiro25@gmail.com>
2022-04-26 18:38:17 +0900

    test.shにコメント追加

--- test.sh
+++ test.sh
@@ -3,9 +3,17 @@
 webserv="./webserv"
 testdir="./webtest"
 
+# バックグラウンドでwebserv実行(出力が邪魔するので/dev/nullへ)
 ($webserv > /dev/null 2>&1) &
+
+# webservのプロセスIDを取得
+# ps | grep webserv で取得できるのは以下２つあるので, sedとか使ってます.
+#  1. webservプロセス
+#  2. webservをgrepするプロセス
 webserv_pid=`ps | grep webserv | sed '/grep/d' | awk '{print $1}'`
 
+# webtestディレクトリ内のgoのテストを走らせる
 make run -C $testdir
 
+# webservを終了させる
 kill $webserv_pid
