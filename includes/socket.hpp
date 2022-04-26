6f44e05ca7406682f1fb606b842b754560fbf314
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-26 18:58:58 +0900

    socketをクラスから関数に変更

---
+++ includes/socket.hpp
@@ -0,0 +1,16 @@
+#ifndef INCLUDES_SOCKET_HPP
+#define INCLUDES_SOCKET_HPP
+
+#include <fcntl.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "ServerConfig.hpp"
+#include "util.hpp"
+
+int              open_new_socket(const ServerConfig &config);
+struct addrinfo *getaddrinfo_via_serv(const ServerConfig &conf);
+
+#endif /* INCLUDES_SOCKET_HPP */
---
+++ srcs/socket.cpp
@@ -0,0 +1,88 @@
+#include "socket.hpp"
+
+/*
+ * リスニングソケットを作成する
+ * int socket(int domain, int type, int protocol);
+ *
+ * macで扱っているプロトコルとプロトコル番号が知りたい場合は
+ * `cat /etc/protocols` で確認できる
+ *
+ * socket(AF_INET, SOCK_STREAM, 0);
+ * AF_INET      IPv4 インターネットドメイン
+ * SOCK_STREAM
+ * シーケンス、信頼性の高い、双方向接続ベースのバイトストリームを提供します。
+ * 詳解UNIXでは「引数「protocol」は普通ゼロで、指定したドメインとソケット種別に
+ * たいするデフォルトのプロトコルを選択します。」と書いてある。なぜ0になるのかわからない
+ */
+static int get_listen_fd(struct addrinfo *info) {
+  int listen_fd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+  if (listen_fd == -1) {
+    error_log_with_errno("socket() failed.");
+    exit(EXIT_FAILURE);
+  }
+  if (fcntl(listen_fd, F_SETFL, O_NONBLOCK) == -1) {
+    error_log_with_errno("fcntl() failed.");
+    exit(EXIT_FAILURE);
+  }
+  int on = 1;
+  if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&on,
+                 sizeof(on)) == -1) {
+    error_log_with_errno("setsockopt() failed.");
+    close(listen_fd);
+    exit(EXIT_FAILURE);
+  }
+  return listen_fd;
+}
+
+/*
+ * ソケットを関連付ける
+ * int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+ * sockfd 　ソケットディスクリプタ
+ * addr    ソケットアドレス
+ * addrlen ソケットアドレス長
+ */
+static void bind_socket(int listen_fd, struct addrinfo *info) {
+  if (bind(listen_fd, info->ai_addr, info->ai_addrlen) == -1) {
+    error_log_with_errno("bind() failed.");
+    close(listen_fd);
+    exit(EXIT_FAILURE);
+  }
+}
+
+/*
+ * 接続を待つ
+ * int listen(int sockfd, int backlog);
+ * sockfd 　ソケットディスクリプタ
+ * backlog 　待ち受けキューの最大数
+ */
+static void listen_passive_socket(int listen_fd) {
+  if (listen(listen_fd, SOMAXCONN) == -1) {
+    error_log_with_errno("listen() failed.");
+    close(listen_fd);
+    exit(EXIT_FAILURE);
+  }
+}
+
+int open_new_socket(const ServerConfig &config) {
+  struct addrinfo *info      = getaddrinfo_via_serv(config);
+  int              listen_fd = get_listen_fd(info);
+  bind_socket(listen_fd, info);
+  listen_passive_socket(listen_fd);
+  freeaddrinfo(info);
+  return listen_fd;
+}
+
+struct addrinfo *getaddrinfo_via_serv(const ServerConfig &conf) {
+  struct addrinfo *info;
+  struct addrinfo  hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family   = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  int error         = getaddrinfo(conf.listen_address_.c_str(),
+                                  conf.listen_port_.c_str(), &hints, &info);
+  if (error) {
+    std::cerr << "getaddrinfo: " << gai_strerror(error) << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  return info;
+}
