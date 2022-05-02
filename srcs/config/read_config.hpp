5785c1e1b66596782ba26b1cb55adf225fead17d
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-02 19:25:21 +0900

    include Guardを訂正

--- srcs/config/read_config.hpp
+++ srcs/config/read_config.hpp
@@ -1,9 +1,9 @@
-#ifndef SRCS_READ_CONFIG_HPP
-#define SRCS_READ_CONFIG_HPP
+#ifndef SRCS_CONFIG_READ_CONFIG_HPP
+#define SRCS_CONFIG_READ_CONFIG_HPP
 
 #include "../config/ServerConfig.hpp"
 #include <vector>
 
 std::vector<ServerConfig> read_config(const char *config_file_path);
 
-#endif /* SRCS_READ_CONFIG_HPP */
+#endif /* SRCS_CONFIG_READ_CONFIG_HPP */
