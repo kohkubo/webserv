dee9587d39787a91577c97a9d14f5761dbddb58c
rakiyama <ryojiro25@gmail.com>
2022-05-02 13:44:24 +0900

    webservが動いているかだけみる

--- webtest/webtest.sh
+++ webtest/webtest.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # バックグラウンドでwebserv実行(出力が邪魔するので/dev/nullへ)
-(cd ../ && ./webserv > /dev/null 2>&1) &
+#(cd ../ && ./webserv > /dev/null 2>&1) &
+cd ../ && ./webserv > /dev/null 2>&1
 
 # webservのプロセスIDを取得
 # ps | grep webserv で取得できるのは以下２つあるので, sedとか使ってます.
 #  1. webservプロセス
 #  2. webservをgrepするプロセス
-webserv_pid=`ps | grep webserv | sed '/grep/d' | awk '{print $1}'`
+#webserv_pid=`ps | grep webserv | sed '/grep/d' | awk '{print $1}'`
 
 # goのテストを走らせる
 #./webtest
