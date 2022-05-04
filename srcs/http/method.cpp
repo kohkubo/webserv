1a2b281e09b3e489bb521dcf532a0164d5cb4458
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-05-04 23:43:47 +0900

    変数名変更

--- srcs/http/method.cpp
+++ srcs/http/method.cpp
@@ -15,9 +15,9 @@ bool is_request_error(HttpMessage &request_message) {
   return false;
 }
 
-static HttpStatusCode check_url(const char *target_url) {
-  if (is_file_exists(target_url)) {
-    if (access(target_url, R_OK) == 0) {
+static HttpStatusCode check_url(const char *target_filepath) {
+  if (is_file_exists(target_filepath)) {
+    if (access(target_filepath, R_OK) == 0) {
       return OK_200;
     } else {
       return FORBIDDEN_403; // TODO: Permission error が 403なのか確かめてない
@@ -27,8 +27,8 @@ static HttpStatusCode check_url(const char *target_url) {
 }
 
 static void set_response_body(http_message_map &response_message,
-                              const char       *target_url) {
-  std::string content            = read_file_tostring(target_url);
+                              const char       *target_filepath) {
+  std::string content            = read_file_tostring(target_filepath);
   response_message[BODY]         = content;
   response_message[CONTENT_LEN]  = to_string(content.size());
   response_message[CONTENT_TYPE] = TEXT_HTML;
@@ -57,24 +57,25 @@ http_message_map method_get(const ServerConfig &server_config,
     response_message[PATH] = BAD_REQUEST_PAGE;
     return response_message;
   }
-  std::string target_url = resolve_url(server_config, request_message.url_);
-  switch (check_url(target_url.c_str())) {
+  std::string target_filepath =
+      resolve_url(server_config, request_message.url_);
+  switch (check_url(target_filepath.c_str())) {
   case OK_200:
     response_message[STATUS_PHRASE] = STATUS_200_PHRASE;
     break;
   case FORBIDDEN_403:
     response_message[STATUS_PHRASE] = STATUS_403_PHRASE;
-    target_url                      = FORBIDDEN_PAGE;
+    target_filepath                 = FORBIDDEN_PAGE;
     break;
   case NOT_FOUND_404:
     response_message[STATUS_PHRASE] = STATUS_404_PHRASE;
-    target_url                      = NOT_FOUND_PAGE;
+    target_filepath                 = NOT_FOUND_PAGE;
     break;
   default:
     response_message[STATUS_PHRASE] = STATUS_520_PHRASE;
-    target_url                      = UNKNOWN_ERROR_PAGE;
+    target_filepath                 = UNKNOWN_ERROR_PAGE;
     break;
   }
-  set_response_body(response_message, target_url.c_str());
+  set_response_body(response_message, target_filepath.c_str());
   return response_message;
 }
