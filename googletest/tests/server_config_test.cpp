383b59117ca65be3917f279e4e61eea67a91e339
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-05-08 21:52:32 +0900

    is_include_same_server_name関数

--- googletest/tests/server_config_test.cpp
+++ googletest/tests/server_config_test.cpp
@@ -226,4 +226,5 @@ TEST(server_config_test, server_group_test) {
   EXPECT_EQ(server_group[1][0]->listen_port_, "5001");
   EXPECT_EQ(server_group.size(), 2);
   EXPECT_EQ(server_group[0].size(), 2);
+  EXPECT_EQ(server_group[1].size(), 1);
 }
