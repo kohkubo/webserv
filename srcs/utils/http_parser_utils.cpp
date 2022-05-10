a5c59f3b24028afe64fffd7fe02483b177b04966
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-05-10 10:45:32 +0900

    comment削除

--- srcs/utils/http_parser_utils.cpp
+++ srcs/utils/http_parser_utils.cpp
@@ -9,7 +9,7 @@ std::string resolve_url(const ServerConfig &server_config,
                         const std::string  &request_url) {
   // TODO: rootの末尾に/入ってるとき
   if (request_url == "/") {
-    return server_config.root_ + "/" + server_config.index_; // "/"iru?
+    return server_config.root_ + "/" + server_config.index_;
   } else {
     return server_config.root_ + "/" + request_url;
   }
