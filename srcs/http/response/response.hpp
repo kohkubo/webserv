713c89e642a58763b254973c9d4e35883ed169a5
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-11 14:29:20 +0900

    add: read_file_tostring_cgi

--- srcs/http/response/response.hpp
+++ srcs/http/response/response.hpp
@@ -7,7 +7,7 @@
 
 http_message_map create_response_info(const ServerConfig &server_config,
                                       HttpMessage        &request_info);
-
 std::string      make_message_string(http_message_map &response_info);
+std::string      read_file_tostring_cgi(const std::string &file_path);
 
 #endif /* SRCS_HTTP_RESPONSE_HPP */
