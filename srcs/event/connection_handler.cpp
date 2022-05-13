c92c7488dd2e6720275125a984395dd31e3fef40
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-05-13 21:57:16 +0900

    クライアントから接続が切れたときの処理を追加

--- srcs/event/connection_handler.cpp
+++ srcs/event/connection_handler.cpp
@@ -7,23 +7,24 @@
 
 // std::string(std::vector<char>)でnull文字交じりのstring作れるかも
 
-static std::string receive_data(int accfd) {
-  const int         buf_size = 1;
+const int buf_size = 2048;
+
+void      connection_receive_handler(int                   accfd,
+                                     connection_list_type &connection_list,
+                                     socket_list_type     &socket_list) {
+  Connection       &connection = connection_list[accfd];
   std::vector<char> buf(buf_size);
   ssize_t           rc = recv(accfd, &buf[0], buf_size, MSG_DONTWAIT);
   if (rc == -1) {
     error_log_with_errno("recv() failed.");
-    return std::string();
   }
-  // TODO: rc == 0 => connection close
-  return std::string(buf.begin(), buf.begin() + rc);
-}
-
-void connection_receive_handler(int                   accfd,
-                                connection_list_type &connection_list,
-                                socket_list_type     &socket_list) {
-  Connection &connection = connection_list[accfd];
-  std::string data       = receive_data(accfd);
+  if (rc == 0) {
+    std::cerr << "closed from client." << std::endl;
+    close(accfd);
+    connection_list.erase(accfd);
+    return;
+  }
+  std::string data = std::string(buf.begin(), buf.begin() + rc);
   connection.parse_buffer(data);
   // cgi用のstateが必要になるかも
   connection.make_response(socket_list[connection.socket_fd_]);
@@ -42,7 +43,7 @@ void connection_send_handler(int accfd, connection_list_type &connection_list) {
     // TODO: 送信完了したrequestがconnection:closeだったら、Connectionを削除。
     connection.request_queue_.pop_front();
     // tmp
-    close(accfd);
+    shutdown(accfd, SHUT_WR);
     connection_list.erase(accfd);
   }
 }
