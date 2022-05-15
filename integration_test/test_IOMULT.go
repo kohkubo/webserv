79d0b7150d83f1b65210070316dfd5a26398c1e5
rakiyama <ryojiro25@gmail.com>
2022-05-15 18:44:14 +0900

    コメント追加

--- integration_test/test_IOMULT.go
+++ integration_test/test_IOMULT.go
@@ -12,6 +12,34 @@ import (
 	"time"
 )
 
+// リクエストとレスポンスの表示
+//func main() {
+//    req, err := http.NewRequest(http.MethodGet, serverAddr, nil)
+//    if err != nil {
+//        log.Fatal(err)
+//    }
+//    req.Header.Add("test-header", "test-header-value")
+
+//    reqDump, err := httputil.DumpRequestOut(req, true)
+//    if err != nil {
+//        log.Fatal(err)
+//    }
+
+//    fmt.Printf("REQUEST:\n%s", string(reqDump))
+
+//    resp, err := http.DefaultClient.Do(req)
+//    if err != nil {
+//        log.Fatal(err)
+//    }
+
+//    respDump, err := httputil.DumpResponse(resp, true)
+//    if err != nil {
+//        log.Fatal(err)
+//    }
+
+//    fmt.Printf("RESPONSE:\n%s", string(respDump))
+//}
+
 // コネクションを確立, connを通して送受信できる
 func connect(port string) net.Conn {
 	conn, err := net.Dial("tcp", "localhost:"+port)
