95334c7331d06cb552fe5f2b8a3af305ade2f31c
rakiyama <ryojiro25@gmail.com>
2022-05-16 11:06:03 +0900

    クラス化

--- integration_test/test_IOMULT.go
+++ integration_test/test_IOMULT.go
@@ -32,39 +32,22 @@ const (
 	RESET = "\033[0m"
 )
 
-// コネクションを確立, connを通して送受信できる
-func connect(port string) net.Conn {
-	conn, err := net.Dial("tcp", "localhost:"+port)
-	if err != nil {
-		log.Fatalf("failt to connect: %v", err)
-	}
-	return conn
-}
-
-// connを通してリクエスト文字列reqを送る
-func send_request(conn net.Conn, req string) {
-	_, err := fmt.Fprintf(conn, req)
-	time.Sleep(1 * time.Millisecond)
-	if err != nil {
-		log.Fatalf("failt to send request: %v", err)
-	}
-}
-
-// connを通してリクエストを受け取る, パースする
-func recv_response(conn net.Conn, method string) *http.Response {
-	r := bufio.NewReader(conn)
+// src(conn)からリクエストを受け取りパースする
+// TODO: タイムアウト実装
+func getResponse(src io.Reader, method string) (*http.Response, error) {
+	r := bufio.NewReader(src)
 	req := &http.Request{
 		Method: method,
 	}
 	resp, err := http.ReadResponse(r, req)
 	if err != nil {
-		log.Fatalf("failt to read response: %v", err)
+		return nil, fmt.Errorf("failt to read response: %w", err)
 	}
-	return resp
+	return resp, nil
 }
 
 // レスポンスが期待するヘッダーとボディを持っているか確認
-func compare_response(resp *http.Response, expectHeader http.Header, expectBody []byte) int {
+func compareResponse(resp *http.Response, expectHeader http.Header, expectBody []byte) (int, error) {
 	var diff_flag int
 	for expect_k, expect_v := range expectHeader {
 		if actual_v, exist := resp.Header[expect_k]; !exist {
@@ -78,71 +61,147 @@ func compare_response(resp *http.Response, expectHeader http.Header, expectBody 
 	if expectBody != nil {
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
-			log.Fatalf("failt to read response: %v", err)
+			return 0, fmt.Errorf("failt to read response: %v", err)
 		}
 		if !reflect.DeepEqual(body, expectBody) {
 			fmt.Printf("body diff: actual=%v, expect=%v\n", body, expectBody)
 			diff_flag++
 		}
 	}
-	return diff_flag
+	return diff_flag, nil
 }
 
-// レスポンスの確認
-func check_response(conn net.Conn, method string, expectHeader http.Header, expectBody []byte) int {
-	defer conn.Close()
-	resp := recv_response(conn, method)
-	defer resp.Body.Close()
-	return compare_response(resp, expectHeader, expectBody)
+type Client struct {
+	Port         string
+	ReqPayload   []string
+	Method       string
+	ExpectHeader http.Header
+	ExpectBody   []byte
+	conn         net.Conn
+	resp         *http.Response
 }
 
-// タイムアウト
+// コネクションを確立, connを通して送受信できる
+func (c *Client) connect() {
+	conn, err := net.Dial("tcp", "localhost:"+c.Port)
+	if err != nil {
+		log.Fatalf("connect: %v", err)
+	}
+	c.conn = conn
+}
+
+func (c *Client) sendRequest() {
+	for _, r := range c.ReqPayload {
+		_, err := fmt.Fprintf(c.conn, r)
+		if err != nil {
+			log.Fatalf("sendRequest: %v", err)
+		}
+	}
+}
+
+// 先頭のReqPayloadのみ送信
+func (c *Client) sendPartialRequest() {
+	if len(c.ReqPayload) != 0 {
+		r := c.ReqPayload[0]
+		c.ReqPayload = c.ReqPayload[1:] // 最初の要素を削除
+		_, err := fmt.Fprintf(c.conn, r)
+		time.Sleep(1 * time.Millisecond) // 連続で使用された場合にリクエストが分かれるように
+		if err != nil {
+			log.Fatalf("sendPartialRequest: %v", err)
+		}
+	}
+}
+
+// レスポンスを受ける
+func (c *Client) recvResponse() error {
+	if len(c.ReqPayload) != 0 {
+		return fmt.Errorf("ReqPayload is not empty!")
+	}
+	resp, err := getResponse(c.conn, c.Method)
+	if err != nil {
+		return fmt.Errorf("recvResponse: %w", err)
+	}
+	c.resp = resp
+	return nil
+}
+
+func (c *Client) isExpectedResult() bool {
+	defer c.conn.Close()
+	err := c.recvResponse()
+	if err != nil {
+		log.Fatalf("isExpectedResult: %v", err)
+	}
+	defer c.resp.Body.Close()
+	result, err := compareResponse(c.resp, c.ExpectHeader, c.ExpectBody)
+	if err != nil {
+		log.Fatalf("isExpectedResult: %v", err)
+	}
+	return result == 0
+}
+
 // 複数クライアント(A, B, C)にコネクションと3分割したメッセージを用意して, ランダムに送信する
 func testIOMULT() {
 	fmt.Println("IOMULT test")
 	testHandler("3client", func() (bool, error) {
-		type Client struct {
-			Port         string
-			ReqPayload   []string
-			Conn         net.Conn
-			Resp         *http.Response
-			ExpectHeader http.Header
-			ExpectBody   []byte
+		clientA := &Client{
+			Port: "5500",
+			ReqPayload: []string{
+				"GET /",
+				" HTTP/1.1\r\nHost: localhost:5500\r\nUse",
+				"r-Agent: Go-http-client/1.1\r\nAccept-Encoding: gzip\r\n\r\n",
+			},
+			Method:       "GET",
+			ExpectHeader: nil,
+			ExpectBody:   FileToBytes(HELLO_WORLD_PAGE),
 		}
+		clientB := &Client{
+			Port: "5001",
+			ReqPayload: []string{
+				"GET /nosuch HT",
+				"TP/1.1\r\nHost: localhost:55",
+				"00\r\nUser-Agent: Go-http-client/1.1\r\nAccept-Encoding: gzip\r\n\r\n",
+			},
+			Method:       "GET",
+			ExpectHeader: nil,
+			ExpectBody:   FileToBytes(NOT_FOUND_PAGE),
+		}
+		clientC := &Client{
+			Port: "5001",
+			ReqPayload: []string{
+				"DELETE /nosuch HTTP/1.1\r",
+				"\nHost: localhost:5500\r\nUser-Agent: Go-http-c",
+				"lient/1.1\r\nAccept-Encoding: gzip\r\n\r\n",
+			},
+			Method:       "DELETE",
+			ExpectHeader: nil,
+			ExpectBody:   FileToBytes(NOT_FOUND_PAGE),
+		}
+		connectAll(clientA, clientB, clientC)
 
-		connA := connect("5500")
-		connB := connect("5001")
-		connC := connect("5001")
-
-		msgA_1 := "GET /"
-		msgA_2 := " HTTP/1.1\r\nHost: localhost:5500\r\nUse"
-		msgA_3 := "r-Agent: Go-http-client/1.1\r\nAccept-Encoding: gzip\r\n\r\n"
-
-		msgB_1 := "GET /nosuch HT"
-		msgB_2 := "TP/1.1\r\nHost: localhost:55"
-		msgB_3 := "00\r\nUser-Agent: Go-http-client/1.1\r\nAccept-Encoding: gzip\r\n\r\n"
-
-		msgC_1 := "DELETE /nosuch HTTP/1.1\r"
-		msgC_2 := "\nHost: localhost:5500\r\nUser-Agent: Go-http-c"
-		msgC_3 := "lient/1.1\r\nAccept-Encoding: gzip\r\n\r\n"
-
-		send_request(connA, msgA_1)
-		send_request(connB, msgB_1)
-		send_request(connC, msgC_1)
-		send_request(connB, msgB_2)
-		send_request(connA, msgA_2)
-		send_request(connC, msgC_2)
-		send_request(connB, msgB_3)
-		diff_flag := check_response(connB, "GET", nil, FileToBytes(NOT_FOUND_PAGE))
-		send_request(connC, msgC_3)
-		diff_flag += check_response(connC, "DELETE", nil, FileToBytes(NOT_FOUND_PAGE))
-		send_request(connA, msgA_3)
-		diff_flag += check_response(connA, "GET", nil, FileToBytes(HELLO_WORLD_PAGE))
-
-		if diff_flag == 0 {
-			return true, nil
-		} else {
+		clientA.sendPartialRequest()
+		clientB.sendPartialRequest()
+		clientC.sendPartialRequest()
+		clientB.sendPartialRequest()
+		clientA.sendPartialRequest()
+		clientC.sendPartialRequest()
+		clientB.sendPartialRequest()
+		if !clientB.isExpectedResult() {
 			return false, nil
 		}
+		clientC.sendPartialRequest()
+		if !clientC.isExpectedResult() {
+			return false, nil
+		}
+		clientA.sendPartialRequest()
+		if !clientA.isExpectedResult() {
+			return false, nil
+		}
+		return true, nil
 	})
+}
+
+func connectAll(a ...*Client) {
+	for _, c := range a {
+		c.connect()
+	}
 }
