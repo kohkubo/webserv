7f0196d8ed44215b7ff6f12d19e09e84a99e7ab0
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-20 13:10:56 +0900

    testcase追加

--- test.sh
+++ test.sh
@@ -1,10 +1,28 @@
 echo
 echo "======================"
-echo "normal 5001"
+echo "normal 5000"
+echo "======================"
+curl localhost:5000
+echo
+echo "======================"
+echo "root directive test"
+echo "normal 5001 in dir1"
 echo "======================"
 curl localhost:5001
 echo
 echo "======================"
-echo "normal 5500"
+echo "index directive test"
+echo "normal 5002 index2! in dir1"
 echo "======================"
-curl localhost:5500
+curl localhost:5002
+echo
+echo "======================"
+echo "normal 5001 404"
+echo "======================"
+curl localhost:5000/jlksdfjklfsdjkldfs
+echo
+echo "======================"
+echo "get request path"
+echo "normal 5000 in dir1"
+echo "======================"
+curl localhost:5000/dir1/index2.html
