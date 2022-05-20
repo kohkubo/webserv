e4b2091b5fffba26402de5552878c74d2efb073e
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-20 13:20:17 +0900

    testcaseを整理

--- test.sh
+++ test.sh
@@ -5,18 +5,6 @@ echo "======================"
 curl localhost:5000
 echo
 echo "======================"
-echo "root directive test"
-echo "normal 5001 in dir1"
-echo "======================"
-curl localhost:5001
-echo
-echo "======================"
-echo "index directive test"
-echo "normal 5002 index2! in dir1"
-echo "======================"
-curl localhost:5002
-echo
-echo "======================"
 echo "normal 5001 404"
 echo "======================"
 curl localhost:5000/jlksdfjklfsdjkldfs
@@ -32,3 +20,15 @@ echo "cgi"
 echo "normal 5000 cgi"
 echo "======================"
 curl localhost:5000/cgi.sh
+echo
+echo "======================"
+echo "root directive test"
+echo "normal 5001 in dir1"
+echo "======================"
+curl localhost:5001
+echo
+echo "======================"
+echo "index directive test"
+echo "normal 5002 index2! in dir1"
+echo "======================"
+curl localhost:5002
