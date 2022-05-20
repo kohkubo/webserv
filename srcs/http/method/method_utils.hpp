d4fa691975d600248d2f6cbb7281108796e90bb6
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-20 14:15:09 +0900

    linux対応

--- srcs/http/method/method_utils.hpp
+++ srcs/http/method/method_utils.hpp
@@ -1,6 +1,8 @@
 #ifndef SRCS_HTTP_HTTP_PARSER_UTILS_HPP
 #define SRCS_HTTP_HTTP_PARSER_UTILS_HPP
 
+#include <stdlib.h>
+
 #include "config/Config.hpp"
 #include "http/const/const_response_key_map.hpp"
 #include "http/request/RequestInfo.hpp"
