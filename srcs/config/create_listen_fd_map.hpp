ac91fde3cf9e57bbdf49e2b26d34965d4c8e65cc
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-20 11:48:21 +0900

    include guard

--- srcs/config/create_listen_fd_map.hpp
+++ srcs/config/create_listen_fd_map.hpp
@@ -1,5 +1,5 @@
-#ifndef SRCS_CONFIG_CONFIG_HPP
-#define SRCS_CONFIG_CONFIG_HPP
+#ifndef SRCS_CONFIG_CREATE_LISTEN_FD_MAP_HPP
+#define SRCS_CONFIG_CREATE_LISTEN_FD_MAP_HPP
 
 #include "config/Config.hpp"
 
@@ -7,4 +7,4 @@ int                            open_new_socket(const Config &config);
 serverList                     read_config(const char *config_file_path);
 std::map<listenFd, conf_group> create_socket_map(const serverList &server_list);
 
-#endif /* SRCS_CONFIG_CONFIG_HPP */
+#endif /* SRCS_CONFIG_CREATE_LISTEN_FD_MAP_HPP */
