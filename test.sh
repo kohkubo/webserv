b0c15ce54577b49821c97468eea462ad318b3d30
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-20 13:00:24 +0900

    test用sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+echo
+echo "======================"
+echo "normal 5001"
+echo "======================"
+curl localhost:5001
+echo
+echo "======================"
+echo "normal 5500"
+echo "======================"
+curl localhost:5500
