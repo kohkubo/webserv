7c51486ef92fbfedbba577a552cb18c0803444af
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-05-21 19:56:32 +0900

    test追加

--- srcs/event/Transaction.hpp
+++ srcs/event/Transaction.hpp
@@ -42,12 +42,14 @@ public:
   bool   is_send_completed() {
     return __send_count_ == static_cast<ssize_t>(__response_.size());
   }
+  // test用
+  const Config *get_conf() { return __conf_; }
 
-  void parse_header(const std::string &header);
-  void detect_config(const confGroup &conf_group);
-  void parse_body(const std::string &body);
-  void create_response();
-  void send_response(int socket_fd);
+  void          parse_header(const std::string &header);
+  void          detect_config(const confGroup &conf_group);
+  void          parse_body(const std::string &body);
+  void          create_response();
+  void          send_response(int socket_fd);
 };
 
 #endif /* SRCS_EVENT_TRANSACTION_HPP */
