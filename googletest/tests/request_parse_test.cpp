26f88017d013cee1fcd9a06a7ce602455b112137
rakiyama <ryojiro25@gmail.com>
2022-05-23 15:58:01 +0900

    content_type, content_lengthヘッダのパーステスト追加

--- googletest/tests/request_parse_test.cpp
+++ googletest/tests/request_parse_test.cpp
@@ -42,7 +42,6 @@ TEST(request_parse_test, normal_delete) {
   EXPECT_EQ(info.get_field_value("Accept"), "*/*");
 }
 
-// TODO: 随時, 追加・変更してください
 TEST(request_parse_test, normal_post) {
   std::string str = "POST /target HTTP/1.1\r\n"
                     "Host: 127.0.0.1:5001\r\n"
@@ -50,15 +49,12 @@ TEST(request_parse_test, normal_post) {
                     "Connection: close\r\n"
                     "Accept: */*\r\n"
                     "Content-Type: application/x-www-form-urlencoded\r\n"
-                    "Content-Length: 18"
-                    "\r\n"
-                    "hoge=huga"
-                    "oii=hanya";
+                    "Content-Length: 18\r\n";
 
   RequestInfo info;
   info.parse_request_header(str);
   EXPECT_EQ(info.method_, POST);
-  EXPECT_EQ(info.target_, "/target");
+  EXPECT_EQ(info.uri_, "/target");
   EXPECT_EQ(info.version_, "HTTP/1.1");
   EXPECT_EQ(info.host_, "127.0.0.1");
   EXPECT_EQ(info.port_, "5001");
@@ -69,22 +65,22 @@ TEST(request_parse_test, normal_post) {
   EXPECT_EQ(info.get_field_value("Accept"), "*/*");
 }
 
-TEST(request_parse_test, query_body) {
-  std::string str = "I'm=going"
-                    "&to=become"
-                    "&the=king"
-                    "&of=the"
-                    "&pirates!!";
+// TEST(request_parse_test, query_body) {
+//   std::string str = "I'm=going"
+//                     "&to=become"
+//                     "&the=king"
+//                     "&of=the"
+//                     "&pirates!!";
 
-  RequestInfo info;
-  info.content_type_ = URLENCODED;
-  info.parse_request_body(str);
-  EXPECT_EQ(info.values_[0], "I'm=going");
-  EXPECT_EQ(info.values_[1], "&to=become");
-  EXPECT_EQ(info.values_[2], "&the=king");
-  EXPECT_EQ(info.values_[3], "&of=the");
-  EXPECT_EQ(info.values_[4], "&pirates!!");
-}
+//  RequestInfo info;
+//  info.content_type_ = URLENCODED;
+//  info.parse_request_body(str);
+//  EXPECT_EQ(info.values_[0], "I'm=going");
+//  EXPECT_EQ(info.values_[1], "&to=become");
+//  EXPECT_EQ(info.values_[2], "&the=king");
+//  EXPECT_EQ(info.values_[3], "&of=the");
+//  EXPECT_EQ(info.values_[4], "&pirates!!");
+//}
 
 TEST(request_parse_test, exception_request_line_few_field) {
   std::string str = "GET HTTP/1.1\r\n"
