150adac3bc04523a3803d2fe53e9ea98d6170b55
rakiyama <ryojiro25@gmail.com>
2022-05-26 21:15:29 +0900

    NULL採用

--- srcs/utils/file_io_utils.cpp
+++ srcs/utils/file_io_utils.cpp
@@ -55,7 +55,7 @@ std::string read_dir_tostring(const std::string &file_path) {
   struct dirent           *diread;
   std::vector<std::string> files;
 
-  while ((diread = readdir(dir)) != nullptr) {
+  while ((diread = readdir(dir)) != NULL) {
     files.push_back(diread->d_name);
   }
   closedir(dir);
