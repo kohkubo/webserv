739adda0c47b78db1f22373fc3860d690854e6a3
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-28 13:24:31 +0900

    test.sh

--- test.sh
+++ test.sh
@@ -1,54 +1,56 @@
-echo
-echo "======================"
-echo "normal 5000"
-echo "======================"
-curl localhost:5000
-echo
-echo
-echo "======================"
-echo "normal 5000"
-echo "======================"
-curl localhost:5000/dir1
-echo
-echo "======================"
-echo "normal 5000 404"
-echo "======================"
-curl localhost:5000/jlksdfjklfsdjkldfs
-echo
-echo "======================"
-echo "5001 403"
-echo "normal 5000"
-echo "======================"
+rm -rf output.txt
+touch output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "normal 5000" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5000 >> output.txt
+echo >> output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "normal 5000" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5000/dir1 >> output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "normal 5000 404" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5000/jlksdfjklfsdjkldfs >> output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "5001 403" >> output.txt
+echo "normal 5000" >> output.txt
+echo "======================" >> output.txt
 chmod 000 html/index.html
-curl localhost:5000 -v
+curl localhost:5000 -v >> output.txt
 chmod 744 html/index.html
-echo
-echo "======================"
-echo "get request path"
-echo "normal 5000 in dir1"
-echo "======================"
-curl localhost:5000/dir1/index2.html
-echo
-echo "======================"
-echo "cgi"
-echo "normal 5000 cgi"
-echo "======================"
-curl localhost:5000/cgi.sh
-echo
-echo "======================"
-echo "root directive test"
-echo "normal 5001 in dir1"
-echo "======================"
-curl localhost:5001
-echo
-echo "======================"
-echo "index directive test"
-echo "normal 5002 index2! in dir1"
-echo "======================"
-curl localhost:5002
-echo
-echo "======================"
-echo "error_page directive test 404"
-echo "normal 5003"
-echo "======================"
-curl localhost:5003/dlkdsklsdf
+echo >> output.txt
+echo "======================" >> output.txt
+echo "get request path" >> output.txt
+echo "normal 5000 in dir1" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5000/dir1/index2.html >> output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "cgi" >> output.txt
+echo "normal 5000 cgi" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5000/cgi.sh >> output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "root directive test" >> output.txt
+echo "normal 5001 in dir1" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5001 >> output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "index directive test" >> output.txt
+echo "normal 5002 index2! in dir1" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5002 >> output.txt
+echo >> output.txt
+echo "======================" >> output.txt
+echo "error_page directive test 404" >> output.txt
+echo "normal 5003" >> output.txt
+echo "======================" >> output.txt
+curl localhost:5003/dlkdsklsdf >> output.txt
