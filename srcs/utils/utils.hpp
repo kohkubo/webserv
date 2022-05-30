e788bd33ad46913d3d758e07d7db3a2a718f79f9
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-05-30 14:48:13 +0900

    linter修正

--- srcs/utils/utils.hpp
+++ srcs/utils/utils.hpp
@@ -29,6 +29,6 @@ bool        is_digits(const std::string &str);
 
 std::size_t hexstr_to_size(const std::string &str);
 
-bool is_minus_depth(const std::string &filepath);
+bool        is_minus_depth(const std::string &filepath);
 
 #endif /* SRCS_UTILS_UTILS_HPP */
