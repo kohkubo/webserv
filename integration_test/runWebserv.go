ece32e6f6a70f3e7ebaf38e1b107b558dada11e6
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-05-30 14:17:06 +0900

    err出力をコンソールに出力

--- integration_test/runWebserv.go
+++ integration_test/runWebserv.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"os/exec"
 	"time"
 )
 
 var current_process *exec.Cmd = nil
+var stderr io.ReadCloser
 
 // 指定したpathのconfigファイルでwebservを立ち上げる。
 func RestartWebserv(configPath string) {
@@ -15,6 +18,7 @@ func RestartWebserv(configPath string) {
 	current_process = exec.Command("./webserv", configPath)
 	// itestの実行ファイルがintegration_test/integration_testを期待
 	current_process.Dir = "../"
+	stderr, _ = current_process.StderrPipe()
 	current_process.Start()
 	time.Sleep(time.Millisecond * 10)
 }
@@ -22,5 +26,8 @@ func RestartWebserv(configPath string) {
 func KillWebserv() {
 	current_process.Process.Kill()
 	current_process.Wait()
+	// if err
+	str, _ := io.ReadAll(stderr)
+	fmt.Printf("%s\n", str)
 	current_process = nil
 }
