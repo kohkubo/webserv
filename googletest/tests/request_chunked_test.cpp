7ac375bbd086331ac05df2104a4d84812086c9fc
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-30 20:16:22 +0900

    セグフォするケースを追加

--- googletest/tests/request_chunked_test.cpp
+++ googletest/tests/request_chunked_test.cpp
@@ -32,3 +32,30 @@ TEST(request_chunked_test, chunked_body) {
   EXPECT_EQ(info.values_[0], "Mozilla aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
                              "Networkhello world\ntest");
 }
+
+TEST(request_chunked_test, chunked_body_error) {
+  std::string chunked_request = "POST / HTTP/1.1\r\n"
+                                "Host: hogehoge.com\r\n"
+                                "Content-Type: text/plain\r\n"
+                                "Transfer-Encoding: chunked\r\n"
+                                "\r\n"
+                                "8\r\n"
+                                "Mozilla \r\n"
+                                "28\r\n"
+                                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\n"
+                                "8\r\n"
+                                " Network\r\n"
+                                "1100\r\n"
+                                "hello world\ntest\r\n "
+                                "0\r\n"
+                                "\r\n";
+
+  Transaction t(-1);
+
+  t.handle_request(chunked_request);
+  const RequestInfo &info = t.get_request_info();
+
+  EXPECT_EQ(info.is_chunked_, true);
+  EXPECT_EQ(info.values_[0], "Mozilla aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+                             "Networkhello world\ntest");
+}
