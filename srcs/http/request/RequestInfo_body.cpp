09fc25286496bed25fd43c7706679cbbd8c30d85
kohkubo <kohkubo@student.42tokyo.jp>
2022-05-31 22:57:06 +0900

    format

--- srcs/http/request/RequestInfo_body.cpp
+++ srcs/http/request/RequestInfo_body.cpp
@@ -7,21 +7,21 @@
 
 // TODO: chunkedならば先にchenkedパースしてからcontent-typeに合わせたパースかも
 // TODO: content-typeの文法を確認する -> headerのパースで確認すべきかもしれない
-void RequestInfo::parse_request_body(std::string       &request_body,
-                                     ContentType content_type) {
+void RequestInfo::parse_request_body(std::string &request_body,
+                                     ContentType  content_type) {
   switch (content_type) {
-    case CONTENT_TYPE_APPLICATION_X_WWW_FORM_URLENCODED:
-      __parse_request_values(request_body);
-      break;
-    case CONTENT_TYPE_MULTIPART_FORM_DATA:
-      env_values_.push_back(request_body); // tmp
-      break;
-    case CONTENT_TYPE_TEXT_PLAIN:
-      env_values_.push_back(request_body); // tmp
-      break;
-    default:
-      std::cerr << "parse body: unknown content-type" << std::endl; // tmp
-      exit(EXIT_FAILURE);
+  case CONTENT_TYPE_APPLICATION_X_WWW_FORM_URLENCODED:
+    __parse_request_values(request_body);
+    break;
+  case CONTENT_TYPE_MULTIPART_FORM_DATA:
+    env_values_.push_back(request_body); // tmp
+    break;
+  case CONTENT_TYPE_TEXT_PLAIN:
+    env_values_.push_back(request_body); // tmp
+    break;
+  default:
+    std::cerr << "parse body: unknown content-type" << std::endl; // tmp
+    exit(EXIT_FAILURE);
   }
   // htmlを介してpostが送られる場合,
   // 上記3つのどれかになるはず(デフォルトは"application/x-www-form-urlencoded")
