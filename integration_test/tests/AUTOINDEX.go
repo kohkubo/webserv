6aaeedfbdfe73edee0530bcd75ad3699a938a2e0
rakiyama <ryojiro25@gmail.com>
2022-05-31 23:14:13 +0900

    テスト追加

--- integration_test/tests/AUTOINDEX.go
+++ integration_test/tests/AUTOINDEX.go
@@ -5,6 +5,22 @@ import (
 	"net/http"
 )
 
+const autoindex_html = `<!DOCTYPE html>
+<html>
+   <head>
+      <title>Index of /autoindex/</title>
+   </head>
+   <body>
+      <h1>Index of /autoindex/</h1>
+      <ul style="list-style:none">
+        <li><a href="../">../ </a></li>
+        <li><a href="dir2/">dir2/ </a></li>
+        <li><a href="test.html">test.html </a></li>
+        <li><a href="dir1/">dir1/ </a></li>
+    </ul>
+   </body>
+</html>`
+
 func TestAUTOINDEX() {
 	testHandler("simple", func() (bool, error) {
 		clientA, err := tester.NewClient(&tester.Client{
@@ -18,7 +34,7 @@ func TestAUTOINDEX() {
 			},
 			ExpectStatusCode: http.StatusOK,
 			ExpectHeader:     nil,
-			ExpectBody:       nil,
+			ExpectBody:       []byte(autoindex_html),
 		})
 		if err != nil {
 			return false, err
