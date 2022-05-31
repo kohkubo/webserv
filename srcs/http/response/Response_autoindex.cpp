4243595b0e81e1b7f5108949845165fc942bbc6a
rakiyama <ryojiro25@gmail.com>
2022-05-31 09:35:51 +0900

    関数整理

--- srcs/http/response/Response_autoindex.cpp
+++ srcs/http/response/Response_autoindex.cpp
@@ -5,43 +5,35 @@
 
 #include "utils/syscall_wrapper.hpp"
 
-// directory内のファイル・ディレクトリ名をstringに格納して返す
-// TODO: htmlに変換する
-std::string Response::__create_autoindex_body(const std::string &file_path) {
-  DIR                     *dir = xopendir(file_path.c_str());
-  struct dirent           *diread;
-  std::vector<std::string> files;
-
-  // xopendir()が成功している時点でdirは有効なものなので
-  // 他のシステムコールはエラーを起こさないことが前提の実装
+static std::string dir_list_lines(const std::string &file_path) {
+  std::string    lines;
+  DIR           *dir = xopendir(file_path.c_str());
+  struct dirent *diread;
   while ((diread = readdir(dir)) != NULL) {
-    files.push_back(diread->d_name);
+    std::string file_name = diread->d_name;
+    // clang-format off
+    lines += "        <li><a href=\"" + file_name + "\">" + file_name + " </a></li>\n";
+    // clang-format on
   }
   closedir(dir);
+  return lines;
+}
 
-  std::string                        ret;
-  std::vector<std::string>::iterator it = files.begin();
-  for (; it != files.end(); it++) {
-    ret += " " + *it;
-  }
-
-  std::stringstream buffer;
-  buffer << "<!DOCTYPE html>\n"
-         << "<html>\n"
-         << "   <head>\n"
-         << "      <title>Index of " << file_path << "</title>\n"
-         << "   </head>\n"
-         << "   <body>\n"
-         << "      <h1>Index of " << file_path << "</h1>\n"
-         << "      <ul style=\"list-style:none\">\n";
-  buffer << "        <li><a href=\"" << files[0] << "\">" << files[0]
-         << " </a></li>\n";
-  buffer << "        <li><a href=\"" << files[1] << "\">" << files[1]
-         << " </a></li>\n";
-  buffer << "        <li><a href=\"" << files[2] << "\">" << files[2]
-         << " </a></li>\n";
-  buffer << "    </ul>\n"
-         << "   </body>\n"
-         << "</html>";
-  return buffer.str();
+std::string Response::__create_autoindex_body(const std::string &file_path) {
+  std::stringstream buff;
+  // clang-format off
+  buff << "<!DOCTYPE html>\n"
+       << "<html>\n"
+       << "   <head>\n"
+       << "      <title>Index of " << __request_info_.uri_ << "</title>\n"
+       << "   </head>\n"
+       << "   <body>\n"
+       << "      <h1>Index of " << __request_info_.uri_ << "</h1>\n"
+       << "      <ul style=\"list-style:none\">\n"
+       <<          dir_list_lines(file_path)
+       << "    </ul>\n"
+       << "   </body>\n"
+       << "</html>";
+  // clang-format on
+  return buff.str();
 }
