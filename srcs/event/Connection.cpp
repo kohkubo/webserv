e847ba8bbfb214c2b57b6f7d4ee0e4f392d07514
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-06-02 22:53:10 +0900

    名前間違えてた

--- srcs/event/Connection.cpp
+++ srcs/event/Connection.cpp
@@ -28,7 +28,7 @@ void Connection::create_sequential_transaction() {
     }
     __transaction_queue_.push_back(Transaction(__conn_fd_));
   }
-  __last_event_time_ = __set_now();
+  __last_event_time_ = __time_now();
 }
 
 bool Connection::append_receive_buffer() {
@@ -66,7 +66,7 @@ void Connection::send_response() {
   if (transaction.is_send_all()) {
     __transaction_queue_.pop_front();
   }
-  __last_event_time_ = __set_now();
+  __last_event_time_ = __time_now();
 }
 
 void Connection::__check_buffer_length_exception(
--- srcs/event/Connection.hpp
+++ srcs/event/Connection.hpp
@@ -31,14 +31,14 @@ private:
   Connection() {}
   static void __check_buffer_length_exception(std::string &request_buffer,
                                               std::size_t  buffer_limit_length);
-  static std::time_t __set_now() { return std::time(NULL); }
+  static std::time_t __time_now() { return std::time(NULL); }
 
 public:
   Connection(connFd conn_fd, confGroup conf_group)
       : __conn_fd_(conn_fd)
       , __conf_group_(conf_group) {
     __transaction_queue_.push_back(Transaction(conn_fd));
-    __last_event_time_ = __set_now();
+    __last_event_time_ = __time_now();
   }
   ~Connection() {}
 
