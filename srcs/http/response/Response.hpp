182f96e2b8c0894daf0c79d8bc098bbd4c22fa21
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-06-06 13:32:15 +0900

    response_str修正

--- srcs/http/response/Response.hpp
+++ srcs/http/response/Response.hpp
@@ -38,20 +38,19 @@ private:
   __get_proper_location(const std::string           &request_uri,
                                     const std::vector<Location> &locations);
 
-  void        __set_status_phrase();
+  std::string __get_status_phrase();
   void        __set_general_header();
   void        __set_entity_header();
-  void        __set_content_len();
-  void        __set_content_type();
-  void        __make_message_string();
-  void        __make_bodiless_message_string();
+  static const std::string &__get_content_type();
+  std::string               __make_message_string();
+  std::string               __make_bodiless_message_string();
 
-  void        __set_file_path(const std::string &request_uri,
-                              const Location    &location);
-  bool        __is_error_status_code();
-  void        __check_filepath_status(const Location &location);
-  void        __set_error_page_body(const Location &location);
-  void        __set_body();
+  void                      __set_file_path(const std::string &request_uri,
+                                            const Location    &location);
+  bool                      __is_error_status_code();
+  void                      __check_filepath_status(const Location &location);
+  void                      __set_error_page_body(const Location &location);
+  void                      __set_body();
   std::string __create_autoindex_body(const std::string &file_path);
 
   void        __post_method_handler(const Location &location) {
