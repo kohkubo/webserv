d03930ec32125829e5f4fdf97ebaf16f90f455b1
rakiyama <ryojiro25@gmail.com>
2022-06-09 13:27:17 +0900

    tokenvectorを使う

--- srcs/utils/tokenize.cpp
+++ srcs/utils/tokenize.cpp
@@ -1,3 +1,5 @@
+#include "tokenize.hpp"
+
 #include <string>
 #include <vector>
 
@@ -9,11 +11,10 @@ static bool is_skip(const char &c, const std::string &skip) {
   return skip.find(c) != std::string::npos;
 }
 
-std::vector<std::string> tokenize(const std::string &text,
-                                  const std::string &delimiter,
-                                  const std::string &skip) {
-  std::vector<std::string> res;
-  std::string::size_type   pos = 0;
+tokenVector tokenize(const std::string &text, const std::string &delimiter,
+                     const std::string &skip) {
+  tokenVector            res;
+  std::string::size_type pos = 0;
   while (text[pos] != '\0') {
     std::string::size_type i = 0;
     if (is_delimiter(text[pos], delimiter)) {
