5dbb330d44de63d8678f92c9353d784c33fa50cd
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-06-14 15:24:22 +0900

    fix test.py

--- html/cgi_test/test.py
+++ html/cgi_test/test.py
@@ -3,13 +3,14 @@
 import cgi
 
 form = cgi.FieldStorage()
+
+print("HTTP/1.0 200 OK")
+print("Content-type: text/html\n\n")
+print("")
+print("Hello world")
+print("<p>name:", form.getvalue("name", "default"))
+print("<p>sex:", form.getvalue("sex", "default"))
+print("<p>blood:", form.getvalue("blood", "default"))
+print("<p>end</p>")
+
 cgi.test()
-
-# print("HTTP/1.0 200 OK")
-# print("Content-type: text/html\n\n")
-# print("")
-# print("Hello world")
-# print("<p>name:", form.getvalue("name", "default"))
-# print("<p>addr:", form.getvalue("addr", "default"))
-# print("<p>end</p>")
-
