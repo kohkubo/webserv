9de0562ac4d89cf3c91887ecc7a2ecdb488b8906
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-06-14 19:46:58 +0900

    newしたメモリをdelete

--- srcs/http/response/ResponseGen_cgi.cpp
+++ srcs/http/response/ResponseGen_cgi.cpp
@@ -1,5 +1,6 @@
 #include "http/response/ResponseGenerator.hpp"
 
+#include <cstring>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -37,17 +38,30 @@ create_environ_map(const std::string &path, const RequestInfo &request_info) {
   return environ_map;
 }
 
-static char *const *
+static char *my_strdup(std::string str) {
+  char *res = new char[str.size() + 1];
+  strncpy(res, str.c_str(), str.size());
+  res[str.size()] = '\0';
+  return res;
+}
+
+static char **
 create_cgi_environ(std::map<std::string, std::string> environ_map) {
   char **cgi_environ = new char *[environ_map.size() + 1];
   std::map<std::string, std::string>::iterator it = environ_map.begin();
   for (std::size_t i = 0; it != environ_map.end(); i++, it++) {
-    // newしたポインタ消失してるので要修正
-    std::string *value = new std::string(it->first + "=" + it->second);
-    cgi_environ[i]     = const_cast<char *>(value->c_str());
+    std::string value = it->first + "=" + it->second;
+    cgi_environ[i]    = my_strdup(value);
   }
   cgi_environ[environ_map.size()] = NULL;
   return cgi_environ;
+}
+
+static void delete_cgi_environ(char **cgi_environ) {
+  for (size_t i = 0; cgi_environ[i] != NULL; i++) {
+    delete cgi_environ[i];
+  }
+  delete[] cgi_environ;
 }
 
 // TODO: error処理
@@ -70,14 +84,15 @@ ResponseGenerator::__read_file_tostring_cgi(const std::string &path,
     dup2(pipefd[WRITE_FD], STDOUT_FILENO);
     close(pipefd[WRITE_FD]);
     // cgiスクリプトを実行可能ファイルとして扱う
-    char *const argv[] = {const_cast<char *>(path.c_str()), NULL};
+    char *argv[] = {const_cast<char *>(path.c_str()), NULL};
     // TODO: request_info.env_valueはパースせずに標準出力へ
     std::map<std::string, std::string> environ_map =
         create_environ_map(path, request_info);
-    char *const *cgi_environ = create_cgi_environ(environ_map);
+    char **cgi_environ = create_cgi_environ(environ_map);
     // TODO: execveの前にスクリプトのあるディレクトリに移動
+    // error handling
     execve(path.c_str(), argv, cgi_environ);
-    delete[] cgi_environ;
+    delete_cgi_environ(cgi_environ);
     exit(0);
   }
   // parent
