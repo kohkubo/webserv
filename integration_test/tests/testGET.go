72b10a221c422cadb18fab9ea460d8f9fc10d4ac
rakiyama <ryojiro25@gmail.com>
2022-06-15 14:30:12 +0900

    リファクタ

--- integration_test/tests/testGET.go
+++ integration_test/tests/testGET.go
@@ -3,7 +3,6 @@ package tests
 import (
 	"integration_test/httptest/client"
 	"integration_test/httptest/wholecheck"
-	"integration_test/client"
 	"integration_test/response"
 	"integration_test/tester"
 	"net/http"
