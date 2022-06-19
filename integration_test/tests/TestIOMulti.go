749a3bfc140ea057d4b5017eb78f2cb55f27a93d
rakiyama <ryojiro25@gmail.com>
2022-06-19 18:13:26 +0900

    テストケース変更

--- integration_test/tests/TestIOMulti.go
+++ integration_test/tests/TestIOMulti.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"integration_test/httpresp"
 	"integration_test/httptest"
 	"net/http"
 )
@@ -16,6 +17,7 @@ var testIOMulti = testCatergory{
 				port := "50000"
 				expectStatusCode := 200
 				expectBody := fileToBytes("html/index.html")
+				// GET /
 				clients = append(clients, httptest.NewClient(httptest.TestSource{
 					Port: port,
 					Request: "GET / HTTP/1.1\r\n" +
@@ -34,11 +36,12 @@ var testIOMulti = testCatergory{
 				}))
 
 				port = "50000"
-				expectStatusCode = 200
-				expectBody = fileToBytes("html/index.html")
+				expectStatusCode = 404
+				expectBody = httpresp.ErrorBody(expectStatusCode)
+				// GET /no_such_file_404
 				clients = append(clients, httptest.NewClient(httptest.TestSource{
 					Port: port,
-					Request: "GET / HTTP/1.1\r\n" +
+					Request: "GET /no_such_file_404 HTTP/1.1\r\n" +
 						"Host: localhost:" + port + "\r\n" +
 						"Connection: close\r\n" +
 						"User-Agent: curl/7.79.1\r\n" +
@@ -53,18 +56,22 @@ var testIOMulti = testCatergory{
 					ExpectBody: expectBody,
 				}))
 
-				port = "50000"
-				expectStatusCode = 200
-				expectBody = fileToBytes("html/index.html")
+				port = "50003"
+				expectStatusCode = 405
+				expectBody = httpresp.ErrorBody(expectStatusCode)
+				// limit_expect NG 405
 				clients = append(clients, httptest.NewClient(httptest.TestSource{
 					Port: port,
-					Request: "GET / HTTP/1.1\r\n" +
-						"Host: localhost:" + port + "\r\n" +
+					Request: "POST / HTTP/1.1" + "\r\n" +
+						`Host: localhost:` + port + "\r\n" +
 						"Connection: close\r\n" +
-						"User-Agent: curl/7.79.1\r\n" +
+						`User-Agent: curl/7.79.1` + "\r\n" +
 						`Accept: */*` + "\r\n" +
-						"\r\n",
-					ExpectStatusCode: expectStatusCode,
+						`Content-Length: 9` + "\r\n" +
+						`Content-Type: application/x-www-form-urlencoded` + "\r\n" +
+						"\r\n" +
+						"name=hoge",
+					ExpectStatusCode: http.StatusMethodNotAllowed,
 					ExpectHeader: http.Header{
 						"Connection":     {"close"},
 						"Content-Length": {lenStr(expectBody)},
