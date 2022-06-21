99dfb052f7708ea6260954ef76be511c184dd415
rakiyama <ryojiro25@gmail.com>
2022-06-21 18:57:17 +0900

    テスト追加

--- integration_test/tests/TestAutoindex.go
+++ integration_test/tests/TestAutoindex.go
@@ -27,8 +27,10 @@ func makeTestPath() func() {
 	testDir := testRoot + "autoindex/"
 	dir1 := testDir + "dir1/"
 	dir2 := testDir + "dir2/"
+	dir3 := testDir + "dir3/"
 	mkdirExitIfError(dir1)
 	mkdirExitIfError(dir2)
+	mkdirExitIfError(dir3)
 	writeFileExitIfError(dir1+"index.html", content_dir1)
 
 	// dummy for checking if dir listing is sorted
@@ -64,6 +66,7 @@ var testAutoindex = testCatergory{
 						"        <li><a href=\"a_dummy_dir/\">a_dummy_dir/ </a></li>\n" +
 						"        <li><a href=\"dir1/\">dir1/ </a></li>\n" +
 						"        <li><a href=\"dir2/\">dir2/ </a></li>\n" +
+						"        <li><a href=\"dir3/\">dir3/ </a></li>\n" +
 						"        <li><a href=\"a_dummy_file\">a_dummy_file </a></li>\n" +
 						"        <li><a href=\"dummy_file\">dummy_file </a></li>\n" +
 						"    </ul>\n" +
@@ -72,6 +75,43 @@ var testAutoindex = testCatergory{
 				clientA := httptest.NewClient(httptest.TestSource{
 					Port: port,
 					Request: "GET /autoindex/ HTTP/1.1\r\n" +
+						"Host: localhost:" + port + "\r\n" +
+						"User-Agent: curl/7.79.1\r\n" +
+						`Accept: */*` + "\r\n" +
+						"\r\n",
+					ExpectStatusCode: expectStatusCode,
+					ExpectHeader: http.Header{
+						"Content-Length": {lenStr(expectBody)},
+						"Content-Type":   {"text/html"},
+					},
+					ExpectBody: expectBody,
+				})
+				return clientA.DoAndCheck()
+			},
+		},
+		{
+			caseName: "no dir/file",
+			test: func() bool {
+				clear := makeTestPath()
+				defer clear()
+				port := "50001"
+				expectStatusCode := 200
+				expectBody := []byte(
+					"<!DOCTYPE html>\n" +
+						"<html>\n" +
+						"   <head>\n" +
+						"      <title>Index of /autoindex/dir3/</title>\n" +
+						"   </head>\n" +
+						"   <body>\n" +
+						"      <h1>Index of /autoindex/dir3/</h1>\n" +
+						"      <ul style=\"list-style:none\">\n" +
+						"        <li><a href=\"../\">../ </a></li>\n" +
+						"    </ul>\n" +
+						"   </body>\n" +
+						"</html>")
+				clientA := httptest.NewClient(httptest.TestSource{
+					Port: port,
+					Request: "GET /autoindex/dir3/ HTTP/1.1\r\n" +
 						"Host: localhost:" + port + "\r\n" +
 						"User-Agent: curl/7.79.1\r\n" +
 						`Accept: */*` + "\r\n" +
