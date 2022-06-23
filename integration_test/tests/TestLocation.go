6248b46cff5abb6a6ea19479b7902e161c703bb4
rakiyama <ryojiro25@gmail.com>
2022-06-23 20:21:27 +0900

    locationマッチなしテスト

--- integration_test/tests/TestLocation.go
+++ integration_test/tests/TestLocation.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"integration_test/httpresp"
 	"integration_test/httptest"
 	"net/http"
 )
@@ -93,6 +94,31 @@ var testLocation = testCatergory{
 				clientA := httptest.NewClient(httptest.TestSource{
 					Port: port,
 					Request: "GET /location1/dir1/ HTTP/1.1\r\n" +
+						"Host: localhost:" + port + "\r\n" +
+						"Connection: close\r\n" +
+						"User-Agent: curl/7.79.1\r\n" +
+						`Accept: */*` + "\r\n" +
+						"\r\n",
+					ExpectStatusCode: expectStatusCode,
+					ExpectHeader: http.Header{
+						"Connection":     {"close"},
+						"Content-Length": {lenStr(expectBody)},
+						"Content-Type":   {"text/html"},
+					},
+					ExpectBody: expectBody,
+				})
+				return clientA.DoAndCheck()
+			},
+		},
+		{
+			caseName: "no location to match",
+			test: func() bool {
+				port := "50001"
+				expectStatusCode := 404
+				expectBody := httpresp.ErrorBody(expectStatusCode)
+				clientA := httptest.NewClient(httptest.TestSource{
+					Port: port,
+					Request: "GET / HTTP/1.1\r\n" +
 						"Host: localhost:" + port + "\r\n" +
 						"Connection: close\r\n" +
 						"User-Agent: curl/7.79.1\r\n" +
