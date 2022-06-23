b3692f401cd93bfcf33a641d4b0067097c9714b8
rakiyama <ryojiro25@gmail.com>
2022-06-23 13:48:03 +0900

    argでoption test

--- integration_test/main.go
+++ integration_test/main.go
@@ -8,7 +8,15 @@ import (
 	"time"
 )
 
+var doOptionalTest bool
+var option_ok = true
+
 func main() {
+	if len(os.Args) != 1 {
+		if os.Args[1] == "-all" {
+			doOptionalTest = true
+		}
+	}
 	var status int
 	select {
 	case <-time.After(5 * time.Minute):
@@ -29,8 +37,11 @@ func main() {
 func test() chan bool {
 	result := make(chan bool)
 	go func() {
-		t := tests.Generate()
-		result <- t.Test()
+		nomal_ok := tests.NormalTests().Test()
+		if doOptionalTest {
+			option_ok = tests.OptionalTests().Test()
+		}
+		result <- nomal_ok && option_ok
 	}()
 	return result
 }
