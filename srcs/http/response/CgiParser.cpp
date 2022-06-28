f570bd13bcc44a8eeaf9343551471ec961ff8d7a
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-06-28 14:42:07 +0900

    comment

--- srcs/http/response/CgiParser.cpp
+++ srcs/http/response/CgiParser.cpp
@@ -1,5 +1,6 @@
 #include "http/response/CgiParser.hpp"
 
+// 微妙かも、先にContent-Type、Locationのパースを試みた結果、NLが決定される方が良さそう。
 static std::string detect_newline_delimiter(const std::string &cgi_response) {
   std::size_t crlf_pos = cgi_response.find("\r\n");
   if (crlf_pos != std::string::npos) {
