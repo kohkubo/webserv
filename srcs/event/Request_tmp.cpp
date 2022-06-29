b3aa7ff22b7b1ecbe560fa634cf092ab300ebbe9
kohkubo <kohkubo@student.42tokyo.jp>
2022-06-29 14:35:52 +0900

    不要な関数を消去

--- srcs/event/Request_tmp.cpp
+++ srcs/event/Request_tmp.cpp
@@ -2,22 +2,6 @@
 
 #include "utils/file_io_utils.hpp"
 
-// GETの条件分岐をhandle_method()のGETの処理で行うなら,
-// request_infoの外にtarget_path_変数を出して編集可能にすべきと考える rakiyama
-static std::string create_target_path(const std::string &request_target,
-                                      const std::string &request_method,
-                                      const Location    &location) {
-  std::string target_path = location.root_ + request_target;
-  if (request_method == "GET") {
-    if (has_suffix(target_path, "/") &&
-        is_file_exists(target_path + location.index_)) {
-      target_path += location.index_;
-    }
-  }
-  // LOG("create_target_path: " << target_path);
-  return target_path;
-}
-
 void Request::_tmp(const ConfigGroup &config_group) {
   // TODO: 例外の処理を整理したあと、1関数に切り出し予定です。 kohkubo
   _request_info_.config_   = config_group.select_config(_request_info_.host_);
