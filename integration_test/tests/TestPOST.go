0f926d2890bf2ddee2994cc7b5799633a8e5dedf
rakiyama <ryojiro25@gmail.com>
2022-06-29 16:21:03 +0900

    test not allow

--- integration_test/tests/TestPOST.go
+++ integration_test/tests/TestPOST.go
@@ -19,7 +19,6 @@ var testPOST = testCatergory{
 		{
 			caseName: "simple",
 			test: func() bool {
-				// setup file to delete
 				postFilePath := "/tmp/post.txt"                           // httpリクエストで指定するターゲットURI
 				rootRelativePath := "html"                                // configで指定されているrootへの(integration_testからの)相対パス
 				deleteFileRelativePath := rootRelativePath + postFilePath // ターゲットURIへの相対パス
@@ -70,58 +69,34 @@ var testPOST = testCatergory{
 				return true
 			},
 		},
-		//{
-		//	caseName: "no_such_file",
-		//	test: func() bool {
-		//		expectStatusCode := 404
-		//		expectBody := httpresp.ErrorBody(expectStatusCode)
-		//		port := "55000"
-		//		clientA := httptest.NewClient(httptest.TestSource{
-		//			Port: port,
-		//			Request: "POST /no_such_file HTTP/1.1\r\n" +
-		//				"Host: localhost:" + port + "\r\n" +
-		//				"Connection: close\r\n" +
-		//				"User-Agent: curl/7.79.1\r\n" +
-		//				`Accept: */*` + "\r\n" +
-		//				"\r\n",
-		//			ExpectStatusCode: expectStatusCode,
-		//			ExpectHeader: http.Header{
-		//				"Connection":     {"close"},
-		//				"Content-Length": {lenStr(expectBody)},
-		//				"Content-Type":   {"text/html"},
-		//			},
-		//			ExpectBody: expectBody,
-		//		})
-		//		return clientA.DoAndCheck()
-		//	},
-		//},
-		//{
-		//	caseName: "403",
-		//	test: func() bool {
-		//		expectStatusCode := 403
-		//		expectBody := httpresp.ErrorBody(expectStatusCode)
-		//		port := "55000"
-		//		clientA := httptest.NewClient(httptest.TestSource{
-		//			Port: port,
-		//			Request: "POST /000.html HTTP/1.1\r\n" +
-		//				"Host: localhost:" + port + "\r\n" +
-		//				"Connection: close\r\n" +
-		//				"User-Agent: curl/7.79.1\r\n" +
-		//				`Accept: */*` + "\r\n" +
-		//				"\r\n",
-		//			ExpectStatusCode: expectStatusCode,
-		//			ExpectHeader: http.Header{
-		//				"Connection":     {"close"},
-		//				"Content-Length": {lenStr(expectBody)},
-		//				"Content-Type":   {"text/html"},
-		//			},
-		//			ExpectBody: expectBody,
-		//		})
-		//		os.Chmod("html/000.html", 000)
-		//		result := clientA.DoAndCheck()
-		//		os.Chmod("html/000.html", 0755)
-		//		return result
-		//	},
-		//},
+		{
+			caseName: "not allow upload_file",
+			test: func() bool {
+				expectStatusCode := 403
+				expectBody := httpresp.ErrorBody(expectStatusCode)
+				port := "50001"
+				postContent := "posted content by integration test"
+
+				clientA := httptest.NewClient(httptest.TestSource{
+					Port: port,
+					Request: "POST /notallow/tmp HTTP/1.1\r\n" +
+						"Host: localhost:" + port + "\r\n" +
+						"Connection: close\r\n" +
+						"Content-Length: " + lenStr([]byte(postContent)) + "\r\n" +
+						"User-Agent: curl/7.79.1\r\n" +
+						`Accept: */*` + "\r\n" +
+						"\r\n" +
+						postContent,
+					ExpectStatusCode: expectStatusCode,
+					ExpectHeader: http.Header{
+						"Connection":     {"close"},
+						"Content-Length": {lenStr(expectBody)},
+						"Content-Type":   {"text/html"},
+					},
+					ExpectBody: expectBody,
+				})
+				return clientA.DoAndCheck()
+			},
+		},
 	},
 }
