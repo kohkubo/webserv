4cbcd3ce611f23ebc8c54a6cb21d7839a0eee068
kohkubo <kohkubo@student.42tokyo.jp>
2022-07-01 13:26:37 +0900

    ファイル分け

--- srcs/http/response/ResponseGen_message.cpp
+++ srcs/http/response/ResponseGen_message.cpp
@@ -1,7 +1,6 @@
 #include "http/response/ResponseGenerator.hpp"
 
 #include "http/const/const_delimiter.hpp"
-#include "utils/file_io_utils.hpp"
 
 namespace response_generator {
 
@@ -21,34 +20,6 @@ std::string create_default_body_content(const HttpStatusCode &status_code) {
          "provided by webserv\n"
          "    </body>\n"
          "</html>";
-}
-
-ResponseGenerator::Body
-ResponseGenerator::_create_status_code_body(const RequestInfo &request_info) {
-  ResponseGenerator::Body      body;
-  errorPageMap::const_iterator it =
-      request_info.config_.error_pages_.find(_status_code_);
-  if (it != request_info.config_.error_pages_.end()) {
-    std::string file_path = request_info.location_->root_ + it->second;
-    if (!is_file_exists(file_path)) {
-      if (_status_code_ == HttpStatusCode::S_404_NOT_FOUND) {
-        return body;
-      }
-      _status_code_ = HttpStatusCode::S_404_NOT_FOUND;
-      return _create_status_code_body(request_info);
-    }
-    Result<int> result = open_read_file(file_path);
-    if (result.is_err_) {
-      if (_status_code_ == HttpStatusCode::S_500_INTERNAL_SERVER_ERROR) {
-        return body;
-      }
-      _status_code_ = HttpStatusCode::S_500_INTERNAL_SERVER_ERROR;
-      return _create_status_code_body(request_info);
-    }
-    body.action_ = Body::READ;
-    body.fd_     = result.object_;
-  }
-  return body;
 }
 
 static std::string start_line(const HttpStatusCode &status_code) {
