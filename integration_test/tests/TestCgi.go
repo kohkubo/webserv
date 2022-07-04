860ec78c9e0f600236b9fa88d501126f72fbd065
rakiyama <ryojiro25@gmail.com>
2022-07-04 19:45:26 +0900

    indexでcgiの指定

--- integration_test/tests/TestCgi.go
+++ integration_test/tests/TestCgi.go
@@ -39,5 +39,35 @@ var testCgi = testCatergory{
 				return clientA.DoAndCheck()
 			},
 		},
+		{
+			caseName: "indexでcgi指定",
+			test: func() bool {
+				expectBody := []byte(
+					"name= taro\n" +
+						"blood= A\n" +
+						"text= string\n",
+				)
+
+				expectStatusCode := 200
+				port := "50000"
+				clientA := httptest.NewClient(httptest.TestSource{
+					Port: port,
+					Request: "GET /html/?name=taro&blood=A&text=string HTTP/1.1\r\n" +
+						"Host: localhost:" + port + "\r\n" +
+						"Connection: close\r\n" +
+						"User-Agent: curl/7.79.1\r\n" +
+						`Accept: */*` + "\r\n" +
+						"\r\n",
+					ExpectStatusCode: expectStatusCode,
+					ExpectHeader: http.Header{
+						"Connection":     {"close"},
+						"Content-Length": {lenStr(expectBody)},
+						"Content-Type":   {"text/html"},
+					},
+					ExpectBody: expectBody,
+				})
+				return clientA.DoAndCheck()
+			},
+		},
 	},
 }
