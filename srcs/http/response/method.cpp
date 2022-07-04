01dc35b004b3b1027caea953aaa0d7a35e6428ee
kohkubo <kohkubo@student.42tokyo.jp>
2022-07-04 16:06:41 +0900

    format

--- srcs/http/response/method.cpp
+++ srcs/http/response/method.cpp
@@ -97,9 +97,9 @@ POSTリクエストを正常に処理した結果、
         request_info, HttpStatusCode::S_500_INTERNAL_SERVER_ERROR);
   }
   ResponseGenerator::Content content;
-  content.action_      = ResponseGenerator::Content::WRITE;
-  content.fd_          = result.object_;
-  content.str_         = request_info.body_;
+  content.action_ = ResponseGenerator::Content::WRITE;
+  content.fd_     = result.object_;
+  content.str_    = request_info.body_;
   return content;
 }
 
