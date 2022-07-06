fc5a48feec53db87b9eddf917342e13527aaa456
kohkubo <kohkubo@student.42tokyo.jp>
2022-07-06 13:50:56 +0900

    とりあえず

--- srcs/http/response/CgiParser.cpp
+++ srcs/http/response/CgiParser.cpp
@@ -3,87 +3,82 @@
 #include "utils/tokenize.hpp"
 #include "utils/utils.hpp"
 
+static std::string getline_cgi_sub(std::string       &source,
+                                   const std::string &delim, std::size_t pos) {
+  std::string line = source.substr(0, pos);
+  source.erase(0, pos + delim.size());
+  return line;
+}
+
 static Result<std::string> getline_cgi(std::string       &source,
                                        const std::string &delim) {
   std::size_t pos = source.find(delim);
-  if (pos == std::string::npos)
+  if (pos == std::string::npos) {
+    LOG("getline_cgi: delim not found");
     return Error<std::string>();
-  std::string line = source.substr(0, pos);
-  source.erase(0, pos + delim.size());
-  return Ok<std::string>(line);
+  }
+  return Ok<std::string>(getline_cgi_sub(source, delim, pos));
 }
 
-// Content-type: text/plain; charset=iso-8859-1
-Result<std::string> parse_content_type(const CgiParser::HeaderMap &header_map) {
+static std::string
+parse_content_type_sub(const std::string &content_type_line) {
+  tokenVector token_vector = tokenize(content_type_line, ";", ";");
+  return token_vector[0];
+}
+
+static Result<std::string>
+parse_content_type(const CgiParser::HeaderMap &header_map) {
   CgiParser::HeaderMap::const_iterator it = header_map.find("content-type");
   if (it == header_map.end()) {
     return Error<std::string>();
   }
-  std::string content_type = it->second;
-  tokenVector token_vector = tokenize(content_type, ";", ";");
-  return Ok<std::string>(token_vector[0]);
+  return Ok<std::string>(parse_content_type_sub(it->second));
 }
 
-CgiParser::CgiState CgiParser::parse_header(std::string &buffer) {
-  for (;;) {
-    Result<std::string> result = getline_cgi(buffer, new_line_delimiter_);
-    if (result.is_err_) {
-      return HEADER;
-    }
-    std::string line = result.object_;
-    if (line.empty()) {
-      result = parse_content_type(header_map_);
-      if (result.is_err_) {
-        ERROR_LOG("parse_content_type error");
-        return ERROR;
-      }
-      content_type_ = result.object_;
-      return HEADER_END;
-    }
-    std::size_t pos = line.find(":");
-    if (pos == std::string::npos) {
-      return ERROR;
-    }
-    std::string key   = tolower(line.substr(0, pos));
-    std::string value = tolower(trim(line.substr(pos + 1), " "));
-    std::pair<HeaderMap::const_iterator, bool> insert_result =
-        header_map_.insert(std::make_pair(key, value));
-    if (!insert_result.second) {
-      return ERROR;
-    }
-  }
-}
-
-Result<std::size_t>
-parse_content_length(const CgiParser::HeaderMap &header_map) {
-  CgiParser::HeaderMap::const_iterator it = header_map.find("content-length");
-  if (it == header_map.end()) {
-    return Error<std::size_t>();
-  }
-  Result<std::size_t> result = string_to_size(it->second);
+static Result<std::size_t>
+parse_content_length_sub(const std::string &content_length) {
+  Result<std::size_t> result = string_to_size(content_length);
   if (result.is_err_) {
     return Error<std::size_t>();
   }
   return Ok<std::size_t>(result.object_);
 }
 
-CgiParser::CgiState CgiParser::parse_body(std::string &buffer) {
-  Result<std::size_t> result = parse_content_length(header_map_);
-  content_length_            = result.object_;
-  if (buffer.size() >= content_length_) {
-    LOG("parse_body");
-    content_ = buffer.substr(0, content_length_);
-    buffer.erase(0, content_length_);
-    return END;
+static Result<std::size_t>
+parse_content_length(const CgiParser::HeaderMap &header_map) {
+  CgiParser::HeaderMap::const_iterator it = header_map.find("content-length");
+  if (it == header_map.end()) {
+    return Error<std::size_t>();
   }
-  return BODY;
+  return parse_content_length_sub(it->second);
 }
 
-bool is_client_location(const std::string &location) {
+static bool is_client_location(const std::string &location) {
   return location.find("http://") == 0 || location.find("https://") == 0;
 }
 
-CgiParser::ResponseType
+static CgiParser::CgiLocation
+parse_cgi_location_sub(const std::string &location_line) {
+  CgiParser::CgiLocation cgi_location;
+  tokenVector            token_vector = tokenize(location_line, "?", "?");
+  cgi_location.path_                  = token_vector[0];
+  if (token_vector.size() > 1) {
+    cgi_location.query_ = token_vector[1];
+  }
+  return cgi_location;
+}
+
+static Result<CgiParser::CgiLocation>
+parse_cgi_location(const CgiParser::HeaderMap &header_map) {
+  CgiParser::HeaderMap::const_iterator it = header_map.find("location");
+  if (it == header_map.end()) {
+    LOG("cgi location not found");
+    return Error<CgiParser::CgiLocation>();
+  }
+  return Ok<CgiParser::CgiLocation>(parse_cgi_location_sub(it->second));
+}
+
+static CgiParser::ResponseType
 get_cgi_response_type(const CgiParser::HeaderMap &header_map) {
   CgiParser::HeaderMap::const_iterator it = header_map.find("location");
   if (it == header_map.end()) {
@@ -100,19 +95,85 @@ get_cgi_response_type(const CgiParser::HeaderMap &header_map) {
   return CgiParser::LOCAL_REDIR;
 }
 
+static bool has_cgi_body(const CgiParser::HeaderMap &header_map) {
+  CgiParser::HeaderMap::const_iterator it = header_map.find("content-length");
+  return it != header_map.end();
+}
+
+static bool insert_header_map(CgiParser::HeaderMap &header_map,
+                              const std::string &line, std::size_t pos) {
+  std::string key   = line.substr(0, pos);
+  std::string value = trim(line.substr(pos + 1), " ");
+  std::pair<CgiParser::HeaderMap::const_iterator, bool> insert_result =
+      header_map.insert(std::make_pair(key, value));
+  return insert_result.second;
+}
+
+CgiParser::CgiState CgiParser::parse_header(std::string &buffer) {
+  for (;;) {
+    Result<std::string> result = getline_cgi(buffer, "\n");
+    if (result.is_err_) {
+      return HEADER;
+    }
+    std::string line = result.object_;
+    if (line.empty() || line == "\n") {
+      LOG("cgi header end ============");
+      return HEADER_END;
+    }
+    std::size_t pos = line.find(":");
+    if (pos == std::string::npos) {
+      ERROR_LOG("parse_hander: invalid header line");
+      return ERROR;
+    }
+    if (!insert_header_map(header_map_, tolower(line), pos)) {
+      ERROR_LOG("parse_hander: duplicate header line");
+      return ERROR;
+    }
+  }
+}
+
+CgiParser::CgiState CgiParser::parse_body(std::string &buffer) {
+  Result<std::size_t> result = parse_content_length(header_map_);
+  if (result.is_err_) {
+    LOG("parse_body: invalid content-length");
+    return ERROR;
+  }
+  content_length_ = result.object_;
+  if (buffer.size() >= content_length_) {
+    LOG("parse_body");
+    content_ = buffer.substr(0, content_length_);
+    buffer.erase(0, content_length_);
+    return END;
+  }
+  return BODY;
+}
+
+CgiParser::CgiState CgiParser::parse_header_end() {
+  Result<std::string> result = parse_content_type(header_map_);
+  if (result.is_err_) {
+    ERROR_LOG("parse_content_type error");
+    return ERROR;
+  }
+  content_type_  = result.object_;
+  response_type_ = get_cgi_response_type(header_map_);
+  Result<CgiParser::CgiLocation> result_cgi_location =
+      parse_cgi_location(header_map_);
+  if (!result_cgi_location.is_err_) {
+    cgi_location_ = result_cgi_location.object_;
+  }
+  if (has_cgi_body(header_map_)) {
+    LOG("has_cgi_body");
+    return BODY;
+  }
+  return END;
+}
+
 CgiParser::CgiState CgiParser::handle_cgi(std::string &buffer) {
   if (state_ == HEADER) {
     state_ = parse_header(buffer);
   }
   if (state_ == HEADER_END) {
-    response_type_ = get_cgi_response_type(header_map_);
-    CgiParser::HeaderMap::const_iterator it =
-        header_map_.find("content-length");
-    if (it == header_map_.end()) {
-      state_ = END;
-    } else {
-      state_ = BODY;
-    }
+    state_ = parse_header_end();
   }
   if (state_ == BODY) {
     state_ = parse_body(buffer);
--- srcs/http/response/CgiParser.hpp
+++ srcs/http/response/CgiParser.hpp
@@ -80,10 +80,13 @@ public:
     CLIENT_REDIR,
     CLIENT_REDIRDOC,
   };
+  struct CgiLocation {
+    std::string path_;
+    std::string query_;
+  };
   CgiState       state_;
   ResponseType   response_type_;
-  std::string    location_;
-  std::string    new_line_delimiter_;
+  CgiLocation    cgi_location_;
   std::size_t    content_length_;
   std::string    content_type_;
   std::string    content_;
@@ -95,11 +98,11 @@ public:
   CgiParser()
       : state_(HEADER)
       , response_type_(DOCUMENT)
-      , new_line_delimiter_("\n")
       , content_length_(0) {}
   ~CgiParser(){};
   CgiState handle_cgi(std::string &buffer);
   CgiState parse_header(std::string &buffer);
+  CgiState parse_header_end();
   CgiState parse_body(std::string &buffer);
 };
 
