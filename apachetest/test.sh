41f08535a9477aa7b6cc0a70d79ca49850c69014
rakiyama <ryojiro25@gmail.com>
2022-07-10 19:58:41 +0900

    cgi設定追加

--- apachetest/test.sh
+++ apachetest/test.sh
@@ -1,7 +1,13 @@
 set -x
 
-#ゲット
+#GET
 export confpath="./conf/httpd.conf"
 docker-compose up -d
 curl localhost:8080/html/ -v
+docker-compose down
+
+#cgi
+export confpath="./conf/cgi.conf"
+docker-compose up -d
+curl http://localhost:8080/cgi-bin/first.pl
 docker-compose down
