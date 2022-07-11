24da9bb830993bd622cd22a62bab2712df562127
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-07-11 18:55:32 +0900

    add HeaderFieldMap class

---
+++ srcs/http/HeaderFieldMap.hpp
@@ -0,0 +1,22 @@
+#ifndef SRCS_HTTP_HEADERFIELDMAP_HPP
+#define SRCS_HTTP_HEADERFIELDMAP_HPP
+
+#include <map>
+
+class HeaderFieldMap {
+public:
+  typedef std::map<std::string, std::string> field_map_type;
+
+private:
+  field_map_type           _field_map_;
+  static const std::string OWS_;
+
+public:
+  HeaderFieldMap() {}
+  ~HeaderFieldMap() {}
+  bool                           store_new_field(std::string &field_line);
+  field_map_type::iterator       find(const std::string &field_name);
+  field_map_type::const_iterator find(const std::string &field_name) const;
+};
+
+#endif /* SRCS_HTTP_HEADERFIELDMAP_HPP */
