0f2ac3ca696747a5bb1fae8c66fb84c6d6713d55
rakiyama <ryojiro25@gmail.com>
2022-07-13 15:36:41 +0900

    apachtest 整理

--- apachetest/test.sh
+++ apachetest/test.sh
@@ -3,11 +3,12 @@ set -x
 #GET
 export confpath="./conf/httpd.conf"
 docker-compose up -d
-curl localhost:8080/html/ -v
+curl localhost:8080 -v
 docker-compose down
 
 #cgi
 export confpath="./conf/cgi.conf"
 docker-compose up -d
 curl localhost:8080/cgi-bin/cgi_test/simple.py -v
+curl localhost:8080/cgi-bin/cgi_test/status.py -v
 docker-compose down
