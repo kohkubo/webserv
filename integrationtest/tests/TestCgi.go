08be60cd09d47849faff1fc93fdc447b26df4aae
kohkubo <kohkubo@student.42tokyo.jp>
2022-07-13 16:34:12 +0900

    testを追記

--- integrationtest/tests/TestCgi.go
+++ integrationtest/tests/TestCgi.go
@@ -71,6 +71,40 @@ var testCgi = testCatergory{
 			},
 		},
 		{
+			caseName: "postでcgi",
+			test: func() bool {
+				expectBody := []byte(
+					"name= taro\n" +
+						"blood= A\n" +
+						"text= string\n",
+				)
+
+				expectStatusCode := 200
+				port := "50000"
+				clientA := httptest.NewClient(httptest.TestSource{
+					Port: port,
+					Request: "POST /html/cgi_test.py HTTP/1.1\r\n" +
+						"Host: localhost:" + port + "\r\n" +
+						"Connection: close\r\n" +
+						"Content-Length: 29" + "\r\n" +
+						"User-Agent: curl/7.79.1\r\n" +
+						`Accept: */*` + "\r\n" +
+						"\r\n" +
+						"name=taro\n" +
+						"blood=A\n" +
+						"text=string",
+					ExpectStatusCode: expectStatusCode,
+					ExpectHeader: http.Header{
+						"Connection":     {"close"},
+						"Content-Length": {lenStr(expectBody)},
+						"Content-Type":   {"text/html"},
+					},
+					ExpectBody: expectBody,
+				})
+				return clientA.DoAndCheck()
+			},
+		},
+		{
 			caseName: "cgi timeout",
 			test: func() bool {
 				expectStatusCode := 500
