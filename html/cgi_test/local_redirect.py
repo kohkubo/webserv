a820eb449ce5fdb0b05310aed7d9996544f07f8b
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-07-17 15:10:18 +0900

    とりあえずapacheテスト追加（未動作

---
+++ html/cgi_test/local_redirect.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# print("Location: /cgi-bin/cgi_test/redirect/redirect_target.py")
+print("Location: /redirect/redirect_target.py")
+print("")
---
+++ html/cgi_test/redirect/redirect_target.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print("Status: 200 OK")
+print("Content-type: text/html")
+print("")
+print("its redirect target")
