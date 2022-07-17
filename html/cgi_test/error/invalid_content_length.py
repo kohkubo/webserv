38dabb2907d8acdfb60d79ab201360df41fad691
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-07-17 11:49:33 +0900

    test追加

---
+++ html/cgi_test/error/invalid_content_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import cgi
+
+form = cgi.FieldStorage()
+
+# cgiのレスポンスの改行は\n(UNIX,POSIX共通)
+print("Status: 200 OK")
+print("Content-type: text/html")
+print("Content-Length: -11")
+print("")
+print("Hello world")
+print("hogehogehoge")
