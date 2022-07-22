ba1aa8c30085a31ce4da5c29d696d8136d54fa27
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-07-22 21:27:02 +0900

    test修正

--- bad_client_test.sh
+++ bad_client_test.sh
@@ -4,11 +4,13 @@ make
 clang++ client.cpp -o bad_client
 ./webserv conf/webserv.conf &
 pid=$!
+echo $pid
 ./bad_client
 ps pid
-if [ $? -eq 0 ]; then
+ret=$?
+if [ ret -eq 0 ]; then
   kill -9 pid
+  exit 0
 else
   exit 1
 fi
-exit 0
