e970ace45d21468100a2ebedb71cdc92ee0d7bfa
kohkubo <kohkubo@student.42tokyo.jp>
2022-07-23 18:03:04 +0900

    ログを表示させないように変更

--- srcs/socket/ClientSocket.cpp
+++ srcs/socket/ClientSocket.cpp
@@ -46,7 +46,7 @@ SocketMapActions ClientSocket::handle_event(short int revents) {
   SocketMapActions socket_map_actions;
   _timeout_.update_last_event();
   if ((revents & (POLLHUP | POLLERR)) != 0) {
-    LOG("connection was closed by client.");
+    // LOG("connection was closed by client.");
     _add_delete_child_socket(socket_map_actions);
     socket_map_actions.add_action(SocketMapAction::DELETE, _socket_fd_, this);
     return socket_map_actions;
@@ -101,7 +101,7 @@ void ClientSocket::_parse_buffer(SocketMapActions &socket_map_actions) {
       if (response_generator.need_socket()) {
         SocketBase *socket =
             response_generator.create_socket(_response_queue_.back(), this);
-        LOG("add new socket:" << socket->socket_fd());
+        // LOG("add new socket:" << socket->socket_fd());
         socket_map_actions.add_action(SocketMapAction::INSERT,
                                       socket->socket_fd(), socket);
         store_child_socket(socket);
@@ -115,7 +115,7 @@ void ClientSocket::_parse_buffer(SocketMapActions &socket_map_actions) {
     if (response_generator.need_socket()) {
       SocketBase *socket =
           response_generator.create_socket(_response_queue_.back(), this);
-      LOG("add new socket:" << socket->socket_fd());
+      // LOG("add new socket:" << socket->socket_fd());
       socket_map_actions.add_action(SocketMapAction::INSERT,
                                     socket->socket_fd(), socket);
       store_child_socket(socket);
@@ -125,7 +125,7 @@ void ClientSocket::_parse_buffer(SocketMapActions &socket_map_actions) {
 
 void ClientSocket::_add_delete_child_socket(
     SocketMapActions &socket_map_actions) {
-  LOG("reserved deleting all child socket");
+  // LOG("reserved deleting all child socket");
   std::set<SocketBase *>::iterator it = _child_socket_set_.begin();
   for (; it != _child_socket_set_.end(); it++) {
     socket_map_actions.add_action(SocketMapAction::DELETE, (*it)->socket_fd(),
