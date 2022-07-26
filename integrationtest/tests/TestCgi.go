724d716efcd5fd5a62607bd4fa68e2caefbb436c
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-07-27 01:04:41 +0900

    add test

--- integrationtest/tests/TestCgi.go
+++ integrationtest/tests/TestCgi.go
@@ -554,5 +554,34 @@ var testCgi = testCatergory{
 				return clientA.DoAndCheck()
 			},
 		},
+
+		{
+			caseName: "cgi sh test",
+			test: func() bool {
+				expectBody := []byte(
+					"Hello world\n",
+				)
+
+				expectStatusCode := 200
+				port := "50002"
+				clientA := httptest.NewClient(httptest.TestSource{
+					Port: port,
+					Request: "GET /cgi_test/simple.sh HTTP/1.1\r\n" +
+						"Host: localhost:" + port + "\r\n" +
+						"Connection: close\r\n" +
+						"User-Agent: curl/7.79.1\r\n" +
+						`Accept: */*` + "\r\n" +
+						"\r\n",
+					ExpectStatusCode: expectStatusCode,
+					ExpectHeader: http.Header{
+						"Connection":     {"close"},
+						"Content-Length": {lenStr(expectBody)},
+						"Content-Type":   {"text/html"},
+					},
+					ExpectBody: expectBody,
+				})
+				return clientA.DoAndCheck()
+			},
+		},
 	},
 }
